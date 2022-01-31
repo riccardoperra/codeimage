59ff0f31bbb2699e0c289cc1b6f69ca713609ee3
Riccardo Perra <riccardo.perra@icloud.com>
2022-01-31 23:21:58 +0100

    chore: update prettier configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ['xo', 'plugin:solid/recommended'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint', 'solid'],
-  rules: {},
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['xo', 'plugin:solid/recommended'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint', 'solid'],
+	rules: {},
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,24 @@
 import logo from './logo.svg';
 import styles from './App.module.css';
-import {JSX} from "solid-js";
+import {JSX} from 'solid-js';
 
-const App: () => JSX.Element = () => {
-
-
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo"/>
-        <p>
-          Edit a<code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
-};
+const App: () => JSX.Element = () => (
+	<div class={styles.App}>
+		<header class={styles.header}>
+			<img src={logo} class={styles.logo} alt="logo" />
+			<p>
+				Edit a<code>src/App.tsx</code> and save to reload.
+			</p>
+			<a
+				class={styles.link}
+				href="https://github.com/solidjs/solid"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				Learn Solid
+			</a>
+		</header>
+	</div>
+);
 
 export default App;
