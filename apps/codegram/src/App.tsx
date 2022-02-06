51f0082df6256d45b67473f7d70c4a54980cfefc
Riccardo Perra <riccardo.perra@icloud.com>
2022-02-06 12:06:34 +0100

    perf: add CodeMirror lineWrap support, fix base styling

--- apps/codegram/src/App.tsx
+++ apps/codegram/src/App.tsx
@@ -1,45 +1,14 @@
 import styles from './App.module.css';
 import {Terminal} from './components/Terminal/Terminal';
 import {Frame} from './components/Frame/Frame';
-import {EditorView} from '@codemirror/view';
-import {createSignal} from 'solid-js';
-import {CodeMirror} from '@local/solid-codemirror';
+import {CustomEditor} from './components/CustomEditor/CustomEditor';
 
 const App = () => {
-  let editor: HTMLElement;
-  const [value, setValue] = createSignal('test');
-
-  const theme = EditorView.theme(
-    {
-      '&': {
-        fontFamily: 'Source Code Pro, monospace',
-      },
-      '.cm-activeLine': {
-        backgroundColor: 'transparent',
-        color: 'white',
-      },
-      '.cm-content': {
-        fontFamily: 'Source Code Pro, monospace',
-        textAlign: 'left',
-      },
-      '.cm-gutters': {
-        display: 'none',
-      },
-    },
-    {dark: true},
-  );
-
   return (
     <div class={styles.App}>
       <Frame>
         <Terminal>
-          <CodeMirror
-            value={value()}
-            extensions={[theme]}
-            editable={true}
-            basicSetup={true}
-            onChange={setValue}
-          />
+          <CustomEditor />
         </Terminal>
       </Frame>
     </div>
