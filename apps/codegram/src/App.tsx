428fb82a2cfb31d870a97742aff0c6cfd9b9b4bd
Riccardo Perra <riccardo.perra@icloud.com>
2022-02-06 11:43:26 +0100

    perf: add CodeMirror from custom library

--- apps/codegram/src/App.tsx
+++ apps/codegram/src/App.tsx
@@ -1,14 +1,45 @@
 import styles from './App.module.css';
 import {Terminal} from './components/Terminal/Terminal';
-import {CodeMirror} from './components/CodeMirror';
 import {Frame} from './components/Frame/Frame';
+import {EditorView} from '@codemirror/view';
+import {createSignal} from 'solid-js';
+import {CodeMirror} from '@local/solid-codemirror';
 
 const App = () => {
+  let editor: HTMLElement;
+  const [value, setValue] = createSignal('test');
+
+  const theme = EditorView.theme(
+    {
+      '&': {
+        fontFamily: 'Source Code Pro, monospace',
+      },
+      '.cm-activeLine': {
+        backgroundColor: 'transparent',
+        color: 'white',
+      },
+      '.cm-content': {
+        fontFamily: 'Source Code Pro, monospace',
+        textAlign: 'left',
+      },
+      '.cm-gutters': {
+        display: 'none',
+      },
+    },
+    {dark: true},
+  );
+
   return (
     <div class={styles.App}>
       <Frame>
         <Terminal>
-          <CodeMirror />
+          <CodeMirror
+            value={value()}
+            extensions={[theme]}
+            editable={true}
+            basicSetup={true}
+            onChange={setValue}
+          />
         </Terminal>
       </Frame>
     </div>
