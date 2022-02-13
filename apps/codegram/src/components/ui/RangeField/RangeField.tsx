d0cb20e056f0e383977650eef0e8a11db696797c
riccardoperra <riccardo.perra@icloud.com>
2022-02-13 09:39:51 +0100

    feat(ui): add base styling for range field

---
+++ apps/codegram/src/components/ui/RangeField/RangeField.tsx
@@ -0,0 +1,44 @@
+import * as styles from './RangeField.css';
+import {UseTextProps} from '../Text/useText';
+import {Component} from 'solid-js';
+import {
+  DynamicProps,
+  WithRef,
+} from 'solid-headless/dist/types/utils/dynamic-prop';
+import {omitProps} from 'solid-use';
+import {assignInlineVars} from '@vanilla-extract/dynamic';
+
+type RangeFieldProps = {
+  value?: number;
+  onChange?: (value: number) => void;
+  size?: UseTextProps['size'];
+} & WithRef<'input'> &
+  Omit<DynamicProps<'input'>, 'as' | 'ref' | 'onInput' | 'onChange'>;
+
+export const RangeField: Component<RangeFieldProps> = props => {
+  function onChange(e: Event): void {
+    if (props.onChange) {
+      const target = e.target as HTMLInputElement;
+      props.onChange(parseInt(target.value, 10));
+    }
+  }
+
+  const progress = () => {
+    const max = props.max ? Number(props.max) : 100;
+    return `${(Number(props.value) / max) * 100}%`;
+  };
+
+  return (
+    <input
+      class={styles.range}
+      value={props.value}
+      onInput={onChange}
+      onChange={onChange}
+      type={'range'}
+      {...omitProps(props, ['class', 'onInput', 'type', 'value', 'onChange'])}
+      style={assignInlineVars({
+        [styles.rangeVars.rangeProgress]: progress(),
+      })}
+    />
+  );
+};
