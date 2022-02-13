c3183ab694a3c37ca380e68751a0de22697da87b
riccardoperra <riccardo.perra@icloud.com>
2022-02-13 09:39:51 +0100

    feat(ui): add base styling for range field

---
+++ apps/codegram/src/components/ui/RangeField/RangeField.css.ts
@@ -0,0 +1,34 @@
+import {createTheme, style} from '@vanilla-extract/css';
+import {themeVars} from '../../../theme/global.css';
+
+export const [rangeTheme, rangeVars] = createTheme({
+  rangeProgress: '0%',
+});
+
+export const range = style([
+  rangeTheme,
+  {
+    appearance: 'none',
+    position: 'relative',
+
+    '::-webkit-slider-thumb': {
+      appearance: 'none',
+      boxShadow: themeVars.boxShadow.lg,
+      backgroundColor: themeVars.backgroundColor.white,
+      border: `1px solid ${themeVars.borderColor.default}`,
+      borderRadius: themeVars.borderRadius.full,
+      height: '12px',
+      width: '12px',
+      marginTop: 'calc(12px / -2)',
+    },
+
+    '::-webkit-slider-runnable-track': {
+      width: '100%',
+      cursor: 'pointer',
+      borderRadius: 0,
+      border: `1px solid ${themeVars.backgroundColor.gray}`,
+      background: `linear-gradient(90deg, ${themeVars.backgroundColor.blue['500']} ${rangeVars.rangeProgress}, ${themeVars.borderColor.default}  ${rangeVars.rangeProgress})`,
+      height: '2px',
+    },
+  },
+]);
