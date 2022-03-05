f30a85f596d4d8e0a21acff5ed76159e9b411de7
riccardoperra <riccardo.perra@icloud.com>
2022-03-05 15:22:50 +0100

    feat: add pwa support

--- apps/codeimage/src/components/BottomBar/BottomBar.tsx
+++ apps/codeimage/src/components/BottomBar/BottomBar.tsx
@@ -75,17 +75,11 @@ export const BottomBar: Component<BottomBarProps> = props => {
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          stroke-width="2"
+          style={{height: '20px', width: '20px'}}
+          viewBox="0 0 20 20"
+          fill="currentColor"
         >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"
-          />
+          <path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z" />
         </svg>
         Share
       </Button>
