214db2c79a691e6fcd5fd75edd598a9af47eab68
riccardoperra <riccardo.perra@icloud.com>
2022-03-11 21:37:59 +0100

    feat: update suspense

--- apps/codeimage/src/_DesktopApp.tsx
+++ apps/codeimage/src/_DesktopApp.tsx
@@ -1,6 +1,5 @@
-import {createSignal, JSXElement, lazy, Suspense} from 'solid-js';
+import {createSignal, JSXElement, lazy} from 'solid-js';
 import ThemeSwitcher from './components/ThemeSwitcher/ThemeSwitcher';
-import {LoadingOverlay} from './components/LoadingOverlay/LoadingOverlay';
 import Sidebar from './components/Scaffold/Sidebar/Sidebar';
 import EditorSidebar from './components/LeftSidebar/EditorSidebar';
 
@@ -11,31 +10,27 @@ export default function DesktopApp(): JSXElement {
 
   return (
     <>
-      <Suspense
-        fallback={<LoadingOverlay overlay={true} width={128} height={128} />}
-      >
-        <Sidebar>
-          <EditorSidebar />
-        </Sidebar>
+      <Sidebar>
+        <EditorSidebar />
+      </Sidebar>
 
-        <div
-          ref={setPortalHostRef}
-          id={'portal-host'}
-          style={{
-            position: 'relative',
-            width: '0px',
-            height: '0px',
-            // eslint-disable-next-line solid/style-prop
-            'z-index': 10,
-          }}
-        />
+      <div
+        ref={setPortalHostRef}
+        id={'portal-host'}
+        style={{
+          position: 'relative',
+          width: '0px',
+          height: '0px',
+          // eslint-disable-next-line solid/style-prop
+          'z-index': 10,
+        }}
+      />
 
-        <LazyEditor />
+      <LazyEditor />
 
-        <Sidebar>
-          <ThemeSwitcher orientation={'vertical'} />
-        </Sidebar>
-      </Suspense>
+      <Sidebar>
+        <ThemeSwitcher orientation={'vertical'} />
+      </Sidebar>
     </>
   );
 }
