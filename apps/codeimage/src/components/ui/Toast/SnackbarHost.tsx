7093b873f3430d01a298e981650ddbd53a5bc178
Riccardo Perra <perrariccardo0@gmail.com>
2022-03-13 16:55:24 +0100

    Revert "fix: fix snackbar portal host parent"
    This reverts commit a4eaeb51a91d489b9c30b4dbaf601d6976508a11.

--- apps/codeimage/src/components/ui/Toast/SnackbarHost.tsx
+++ apps/codeimage/src/components/ui/Toast/SnackbarHost.tsx
@@ -3,7 +3,6 @@ import {Component, For} from 'solid-js';
 import {SnackBar} from './Snackbar';
 import * as styles from './Snackbar.css';
 import {host} from './Snackbar.css';
-import {PortalHostInjector} from '../PortalHost/PortalHost';
 
 export interface SnackbarData {
   message: string | Component;
@@ -21,33 +20,31 @@ export const NotificationHandler = () => {
   }
 
   return (
-    <PortalHostInjector>
-      <Toaster class={styles.hostWrapper}>
-        <Transition
-          show={notifications().length > 0}
-          class="relative transition"
-          enter="ease-out duration-300"
-          enterFrom="opacity-0 scale-50 translate-y-full"
-          enterTo="opacity-100 scale-100 translate-y-0"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100 scale-100 translate-y-0"
-          leaveTo="opacity-0 scale-50 translate-y-full"
-          afterLeave={clearNotifications}
-        >
-          <div class={host}>
-            <For each={notifications()}>
-              {item => (
-                <SnackBar
-                  id={item.id}
-                  message={item.data.message}
-                  closeable={item.data.closeable}
-                  actions={item.data.actions}
-                />
-              )}
-            </For>
-          </div>
-        </Transition>
-      </Toaster>
-    </PortalHostInjector>
+    <Toaster class={styles.hostWrapper}>
+      <Transition
+        show={notifications().length > 0}
+        class="relative transition"
+        enter="ease-out duration-300"
+        enterFrom="opacity-0 scale-50 translate-y-full"
+        enterTo="opacity-100 scale-100 translate-y-0"
+        leave="ease-in duration-200"
+        leaveFrom="opacity-100 scale-100 translate-y-0"
+        leaveTo="opacity-0 scale-50 translate-y-full"
+        afterLeave={clearNotifications}
+      >
+        <div class={host}>
+          <For each={notifications()}>
+            {item => (
+              <SnackBar
+                id={item.id}
+                message={item.data.message}
+                closeable={item.data.closeable}
+                actions={item.data.actions}
+              />
+            )}
+          </For>
+        </div>
+      </Transition>
+    </Toaster>
   );
 };
