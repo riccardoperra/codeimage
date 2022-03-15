fb017aea6935e897100cd99084bb3b58ab6c7879
riccardoperra <riccardo.perra@icloud.com>
2022-03-15 23:54:43 +0100

    feat: add duotone-dark theme

---
+++ packages/theme/src/lib/themes/duotone-dark/index.ts
@@ -0,0 +1,17 @@
+import {createPrismJsTheme} from '../../prismjs/prismjs-theme-factory';
+import {duotoneDark} from './duotone-dark';
+
+export const duotoneDarkTheme = createPrismJsTheme({
+  id: 'duotoneDark',
+  editorTheme: [],
+  externalStylesheet: duotoneDark,
+  properties: {
+    darkMode: true,
+    label: 'Duotone Dark',
+    previewBackground: '#7840ba',
+    terminal: {
+      main: '#2D2B38',
+      text: '#FFFFFF',
+    },
+  },
+});
