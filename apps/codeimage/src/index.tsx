4db6b90021af1b67f26370324a34c62f06064a24
riccardoperra <riccardo.perra@icloud.com>
2022-03-21 23:42:31 +0100

    feat: perf improvements test

--- apps/codeimage/src/index.tsx
+++ apps/codeimage/src/index.tsx
@@ -3,18 +3,19 @@ import {createI18nContext, I18nContext} from '@codeimage/locale';
 import {locale} from './i18n';
 import './assets/styles/app.scss';
 import {devTools} from '@ngneat/elf-devtools';
-import {lazy, Suspense} from 'solid-js';
+import {Suspense} from 'solid-js';
+import App from './App';
 
 if (import.meta.env.DEV) {
   devTools();
 }
 
-const App = lazy(async () => {
-  return import('./App').then(async e => {
-    await new Promise(resolve => setTimeout(resolve, 0, null));
-    return e;
-  });
-});
+// const App = lazy(async () => {
+//   return import('./App').then(async e => {
+//     await new Promise(resolve => setTimeout(resolve, 0, null));
+//     return e;
+//   });
+// });
 
 const i18n = createI18nContext(locale);
 
