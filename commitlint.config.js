e0a0dee4803ee02ef3e220ac563b7571bd195686
riccardoperra <riccardo.perra@icloud.com>
2022-03-27 20:21:00 +0200

    chore: add commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,18 @@
-module.exports = {extends: ['@commitlint/config-conventional']};
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'app',
+        'ui',
+        'theme',
+        'config',
+        'locale',
+        // Must be used for ci only or deploy commit
+        'release',
+      ],
+    ],
+  },
+};
