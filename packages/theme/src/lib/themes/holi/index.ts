f2d7bc9493b19d09dff664381eafbda5cfc550a7
riccardoperra <riccardo.perra@icloud.com>
2022-04-04 23:07:42 +0200

    fix(theme): add holi dark autocomplete theme

--- packages/theme/src/lib/themes/holi/index.ts
+++ packages/theme/src/lib/themes/holi/index.ts
@@ -1,9 +1,16 @@
 import {holi} from './holi';
 import {createPrismJsTheme} from '../../prismjs/prismjs-theme-factory';
+import {styledAutocomplete} from '../../plugins/autocomplete-style';
 
 export const holiTheme = createPrismJsTheme({
   id: 'holiTheme',
-  editorTheme: [],
+  editorTheme: [
+    styledAutocomplete({
+      background: '#122f6d',
+      selectedBackground: '#1a1a82',
+      selectedColor: '#d6b007',
+    }),
+  ],
   externalStylesheet: holi,
   properties: {
     darkMode: true,
