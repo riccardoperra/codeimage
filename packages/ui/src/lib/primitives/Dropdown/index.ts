19442ddc5ea24192079f9df32ecf194e7447e792
riccardoperra <riccardo.perra@icloud.com>
2022-04-08 00:00:36 +0200

    refactor(ui): #184 move Dropdown to @codeimage/ui

---
+++ packages/ui/src/lib/primitives/Dropdown/index.ts
@@ -0,0 +1,5 @@
+export * as dropdownStyles from './Dropdown.css';
+export * from './DropdownItem';
+export * from './DropdownMenu';
+// export * from './DropdownPanel';
+export * from './DropdownPortal';
