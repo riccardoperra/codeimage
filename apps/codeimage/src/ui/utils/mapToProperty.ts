62b85e5db8d7dbc69faf9e0e8d2c8f50d6007b5f
riccardoperra <riccardo.perra@icloud.com>
2022-04-09 16:14:00 +0200

    chore(app): remove unused helpers

--- apps/codeimage/src/ui/utils/mapToProperty.ts
+++
@@ -1,8 +0,0 @@
-import {Properties} from 'csstype';
-
-export const mapToProperty =
-  <Property extends keyof Properties<string | number>>(property: Property) =>
-  (value: string | number) => {
-    const styleRule = {[property]: value};
-    return styleRule;
-  };
