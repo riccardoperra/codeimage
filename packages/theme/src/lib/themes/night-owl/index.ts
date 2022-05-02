fbad32f7296427a6e37a1a0007416ad2bcff6d62
riccardoperra <riccardo.perra@icloud.com>
2022-05-03 00:09:11 +0200

    refactor(ui): refactor Night owl theme

--- packages/theme/src/lib/themes/night-owl/index.ts
+++ packages/theme/src/lib/themes/night-owl/index.ts
@@ -1,22 +1,13 @@
-import {createPrismJsTheme} from '../../prismjs/prismjs-theme-factory';
+import {createTheme} from '../../core';
 import {nightOwl} from './night-owl';
-import {styledAutocomplete} from '../../plugins/autocomplete-style';
 
-export const nightOwlTheme = createPrismJsTheme({
+export const nightOwlTheme = createTheme({
   id: 'nightOwlTheme',
-  editorTheme: [
-    styledAutocomplete({
-      background: '#011423',
-      border: '#5f7e97',
-      selectedBackground: '#011220',
-      selectedColor: 'rgb(247, 140, 108)',
-    }),
-  ],
-  externalStylesheet: nightOwl,
+  editorTheme: nightOwl,
   properties: {
     darkMode: true,
     label: 'Night Owl',
-    previewBackground: '#091c43',
+    previewBackground: '#15357F',
     terminal: {
       main: '#011627',
       text: '#D0D8E5',
