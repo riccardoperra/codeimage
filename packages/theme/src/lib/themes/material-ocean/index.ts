bf31e495670fd63f2c1da1134e8ec9d167f49735
riccardoperra <riccardo.perra@icloud.com>
2022-05-02 22:56:12 +0200

    refactor(ui): update Material Ocean theme

--- packages/theme/src/lib/themes/material-ocean/index.ts
+++ packages/theme/src/lib/themes/material-ocean/index.ts
@@ -1,1 +1,16 @@
-export * from './theme';
+import {createTheme} from '../../core';
+import {materialOcean, palette} from './theme';
+
+export const materialOceanTheme = createTheme({
+  id: 'materialOcean',
+  editorTheme: materialOcean,
+  properties: {
+    darkMode: true,
+    label: 'Material Ocean',
+    previewBackground: palette.defaultAccent,
+    terminal: {
+      main: palette.backgroundAlt,
+      text: palette.white,
+    },
+  },
+} as const);
