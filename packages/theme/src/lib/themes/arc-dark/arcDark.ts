01175f300ebd061f826710cf9fff682c3a301fe3
riccardoperra <riccardo.perra@icloud.com>
2022-05-05 22:52:12 +0200

    fix: fix arc dark theme

--- packages/theme/src/lib/themes/arc-dark/arcDark.ts
+++ packages/theme/src/lib/themes/arc-dark/arcDark.ts
@@ -1,123 +1,74 @@
 import {HighlightStyle, syntaxHighlighting} from '@codemirror/language';
 import {tags as t} from '@lezer/highlight';
-import {EditorView} from '@codemirror/view';
+import {defineEditorTheme} from '../../core';
 
 export const palette = {
-  background: '#1C1F26',
+  background: '#21252bff',
   gray: '#383c4a',
   grayLight: '#a2a2a2',
+  pink: '#C586C0',
   magenta: '#c35f9b',
+  green1: '#608b4e',
   green: '#61bc3b',
   greenLight: '#91ff58',
   orange: '#f3b43a',
   red: '#d64937',
-  indigo: '#5294e2',
+  red2: '#d16969',
+  indigo: '#569cd6',
+  sea: '#4EC9B0',
   cyanLight: '#37c3d6',
+  yellow1: '#DCDCAA',
+  blue1: '#9CDCFE',
   cyan: '#37c3d6',
   cyanDark: '#288e9c',
   violet: '#a8b4de',
+  gray2: '#d4d4d4',
+  co: '#d7ba7d',
+  orange2: '#ce9178',
 };
 
-const highlightStyle = syntaxHighlighting(
-  HighlightStyle.define([
-    {tag: [t.keyword, t.constant], color: palette.indigo},
-    {tag: [t.string], color: palette.orange},
-    {tag: [t.escape], color: palette.greenLight},
-    {tag: [t.number, t.bool], color: palette.orange},
-    {tag: [t.operator], color: palette.cyan},
-    {tag: [t.brace, t.paren], color: palette.magenta},
-    {tag: [t.comment], color: palette.violet, fontStyle: 'italic'},
-    {tag: [t.special], color: palette.indigo},
-    {tag: [t.className], color: palette.cyanDark},
-    {tag: [t.attributeName], color: palette.red},
-    {tag: [t.attributeValue], color: palette.orange},
-    {
-      tag: [t.function],
-      color: palette.cyanDark,
+export const arcDark = [
+  defineEditorTheme({
+    highlight: {
+      keywords: palette.indigo,
+      strings: palette.orange2,
+      numbers: palette.orange,
+      boolean: palette.orange,
+      operators: palette.gray2,
+      brackets: palette.magenta,
+      paren: palette.magenta,
+      comments: palette.green1,
+      className: palette.cyanDark,
+      attrValue: palette.orange,
+      attrName: palette.co,
+      function: palette.green1,
+      typeName: palette.sea,
+      propertyName: palette.violet,
+      variableName: palette.blue1,
+      moduleKeyword: palette.magenta,
+      tag: palette.violet,
+      base: palette.cyanLight,
+      variableNameSpecial: palette.green1,
+      delimiters: palette.indigo,
+      regexp: palette.red2,
+      self: palette.indigo,
     },
-    {tag: [t.function(t.variableName)], color: palette.cyan},
-    {tag: [t.propertyName], color: palette.indigo},
-    {tag: [t.variableName], color: palette.cyan},
-    {tag: [t.moduleKeyword], color: palette.magenta},
-  ]),
-);
-
-const theme = EditorView.theme(
-  {
-    '&': {
-      color: palette.grayLight,
-      backgroundColor: palette.background,
+    darkMode: true,
+    autocomplete: {
+      background: palette.background,
+      selectedBackground: `${palette.cyan}50`,
     },
-
-    '.cm-content': {
-      caretColor: palette.cyan,
-    },
-
-    '.cm-cursor, .cm-dropCursor': {
-      borderLeftColor: palette.cyan,
-    },
-    '&.cm-focused .cm-selectionBackground, .cm-selectionBackground, .cm-content ::selection':
-      {backgroundColor: `${palette.cyan}25`},
-    '.cm-panels': {
-      backgroundColor: palette.background,
-      color: palette.grayLight,
-    },
-    '.cm-panels.cm-panels-top': {borderBottom: '2px solid black'},
-    '.cm-panels.cm-panels-bottom': {borderTop: '2px solid black'},
-
-    '.cm-searchMatch': {
-      backgroundColor: '#72a1ff59',
-      outline: '1px solid #457dff',
-    },
-
-    '.cm-searchMatch.cm-searchMatch-selected': {
-      backgroundColor: '#6199ff2f',
-    },
-
-    '.cm-activeLine': {backgroundColor: palette.background},
-    '.cm-selectionMatch': {backgroundColor: palette.background},
-
-    '&.cm-focused .cm-matchingBracket, &.cm-focused .cm-nonmatchingBracket': {
-      backgroundColor: `${palette.background}50`,
-      outline: `1px solid ${palette.cyan}50`,
-    },
-
-    '.cm-gutters': {
-      backgroundColor: `${palette.cyan}60`,
+    cursor: {
       color: palette.cyan,
-      border: 'none',
     },
-
-    '.cm-activeLineGutter': {
-      backgroundColor: palette.cyan,
+    selection: {
+      backgroundColor: `${palette.cyan}25`,
     },
-
-    '.cm-foldPlaceholder': {
-      backgroundColor: 'transparent',
-      border: 'none',
-      color: '#ddd',
+    lineNumbers: {
+      color: palette.grayLight,
     },
-
-    '.cm-tooltip': {
-      border: 'none',
-      backgroundColor: palette.background,
-    },
-    '.cm-tooltip .cm-tooltip-arrow:before': {
-      borderTopColor: 'transparent',
-      borderBottomColor: 'transparent',
-    },
-    '.cm-tooltip .cm-tooltip-arrow:after': {
-      borderTopColor: palette.background,
-      borderBottomColor: palette.background,
-    },
-    '.cm-tooltip-autocomplete': {
-      '& > ul > li[aria-selected]': {
-        backgroundColor: `${palette.cyan}50`,
-      },
-    },
-  },
-  {
-    dark: true,
-  },
-);
-export const arcDark = [highlightStyle, theme];
+  }),
+  syntaxHighlighting(
+    HighlightStyle.define([{tag: t.controlKeyword, color: palette.pink}]),
+  ),
+];
