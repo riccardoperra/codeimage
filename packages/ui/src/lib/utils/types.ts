7e17bf32fd85b3bd5cb05950b96e53f5a25494db
riccardoperra <riccardo.perra@icloud.com>
2022-05-16 20:19:47 +0200

    chore: update solid types

--- packages/ui/src/lib/utils/types.ts
+++ packages/ui/src/lib/utils/types.ts
@@ -1,41 +1,32 @@
 import {mergeProps} from 'solid-js';
 
-type Simplify<T> = T extends object ? {[K in keyof T]: T[K]} : T;
-type UnboxLazy<T> = T extends () => infer U ? U : T;
-type RequiredKeys<T> = keyof {
-  [K in keyof T as T extends {[_ in K]: unknown} ? K : never]: 0;
-};
-type Override<T, U> = {
-  // all keys in T which are not overridden by U
-  [K in keyof Omit<T, RequiredKeys<U>>]:
-    | T[K]
-    | Exclude<U[K & keyof U], undefined>;
-} & {
-  // all keys in U except those which are merged into T
-  [K in keyof Omit<U, Exclude<keyof T, RequiredKeys<U>>>]:
-    | Exclude<U[K], undefined>
-    | (undefined extends U[K] ? (K extends keyof T ? T[K] : undefined) : never);
-};
+type Override<T, U> = T extends object
+  ? U extends object
+    ? {
+        [K in keyof T]: K extends keyof U
+          ? undefined extends U[K]
+            ? Exclude<U[K], undefined> | T[K]
+            : U[K]
+          : T[K];
+      } & {
+        [K in keyof U]: K extends keyof T
+          ? undefined extends U[K]
+            ? Exclude<U[K], undefined> | T[K]
+            : U[K]
+          : U[K];
+      }
+    : T & U
+  : T & U;
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 export type MergeProps<T extends unknown[], Curr = {}> = T extends [
-  infer Next,
+  infer Next | (() => infer Next),
   ...infer Rest,
 ]
-  ? MergeProps<
-      Rest,
-      Next extends object
-        ? // eslint-disable-next-line @typescript-eslint/ban-types
-          Next extends Function
-          ? Curr
-          : // This is the line that fix the issue
-          Next extends infer U
-          ? Override<Curr, UnboxLazy<U>>
-          : never
-        : Curr
-    >
-  : Simplify<Curr>;
+  ? MergeProps<Rest, Override<Curr, Next>>
+  : Curr;
 
 export const mergeProps2: <T extends [unknown, ...unknown[]]>(
   ...sources: T
-) => MergeProps<T> = mergeProps as any;
+) => // eslint-disable-next-line @typescript-eslint/no-explicit-any
+MergeProps<T> = mergeProps as any;
