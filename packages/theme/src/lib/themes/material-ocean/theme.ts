92c0f407203c6182cb98045a5990f9156257ebf1
riccardoperra <riccardo.perra@icloud.com>
2022-06-05 16:47:06 +0200

    fix: material ocean theme

--- packages/theme/src/lib/themes/material-ocean/theme.ts
+++ packages/theme/src/lib/themes/material-ocean/theme.ts
@@ -16,7 +16,7 @@ export const palette = {
   brown: '#916b53',
   pink: '#ff9cac',
   violet: '#bb80b3',
-  backgroundAlt: '#192429',
+  backgroundAlt: '#212d33',
   defaultAccent: '#80CBC4',
   background: '#263238',
   comments: '#546E7A',
