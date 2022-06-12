70c9343854b9fddeab815f4d0213f6bf4cf28838
riccardoperra <riccardo.perra@icloud.com>
2022-06-12 16:07:37 +0200

    perf(config): split chunks for codemirror legacy lang

--- packages/config/src/public-api.ts
+++ packages/config/src/public-api.ts
@@ -1,8 +1,13 @@
-export * from './lib/core/createConfiguration';
-export * from './lib/core/ConfigurationProvider';
-export * from './lib/types/configuration';
-export * from './lib/types/language-def';
-export * from './lib/types/terminal-def';
+export {createConfiguration} from './lib/core/createConfiguration';
+export {type AppStaticConfiguration} from './lib/types/configuration';
+export {
+  type LanguageDefinition,
+  type LanguageIconDefinition,
+} from './lib/types/language-def';
+export {
+  type TerminalDefinition,
+  type TerminalDefinitionMap,
+} from './lib/types/terminal-def';
 export {SUPPORTED_LOCALES} from './lib/base/locales';
 export {SUPPORTED_LANGUAGES} from './lib/base/languages';
 export {AVAILABLE_COLORS, AVAILABLE_GRADIENTS} from './lib/base/colors';
