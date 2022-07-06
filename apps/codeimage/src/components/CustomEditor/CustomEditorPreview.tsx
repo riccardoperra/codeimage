a7fe54509de3b199d010e3624a1ec46318772663
riccardoperra <riccardo.perra@icloud.com>
2022-07-06 21:54:16 +0200

    refactor: fix solid-codemirror build

--- apps/codeimage/src/components/CustomEditor/CustomEditorPreview.tsx
+++ apps/codeimage/src/components/CustomEditor/CustomEditorPreview.tsx
@@ -13,7 +13,6 @@ import {
   createMemo,
   createResource,
   on,
-  onCleanup,
   onMount,
   VoidProps,
 } from 'solid-js';
--- apps/codeimage/src/core/hooks/from-observable-object.ts
+++ apps/codeimage/src/core/hooks/from-observable-object.ts
@@ -3,9 +3,10 @@ import {createStore, reconcile} from 'solid-js/store';
 import {select} from '@ngneat/elf';
 import {onCleanup} from 'solid-js';
 
-export function fromObservableObject<T>(store: Observable<T>) {
+// eslint-disable-next-line @typescript-eslint/ban-types
+export function fromObservableObject<T extends {}>(store: Observable<T>) {
   const initialValue = {} as T;
-  const [state, setState] = createStore<T>(initialValue);
+  const [state, setState] = createStore(initialValue);
   const sub = store.pipe(select(state => state)).subscribe(nextState => {
     setState(reconcile(nextState));
   });
