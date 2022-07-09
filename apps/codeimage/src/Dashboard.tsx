9cfdba79c2adba9be1b51ecb7b26c70feb0e23bd
riccardoperra <riccardo.perra@icloud.com>
2022-07-09 15:14:02 +0200

    feat: add dashboard display mode

--- apps/codeimage/src/Dashboard.tsx
+++ apps/codeimage/src/Dashboard.tsx
@@ -1,26 +1,19 @@
-import {Box, Button, Text} from '@codeimage/ui';
+import {
+  Box,
+  Button,
+  SegmentedField,
+  SegmentedFieldItem,
+  Text,
+} from '@codeimage/ui';
 import {SkeletonLine} from '@ui/Skeleton/Skeleton';
 import {SkeletonDivider} from '@ui/Skeleton/SkeletonDivider';
-import {Link, useParams} from 'solid-app-router';
-import {useRoute} from 'solid-app-router/dist/routing';
-import {
-  createEffect,
-  createResource,
-  createSignal,
-  For,
-  on,
-  Show,
-  Suspense,
-} from 'solid-js';
+import {Link} from 'solid-app-router';
+import {createResource, createSignal, For, Show, Suspense} from 'solid-js';
 import {Footer} from './components/Footer/Footer';
 import {CodeIcon} from './components/Icons/Code';
-import {CodeImageLogo} from './components/Icons/CodeImageLogo';
 import {FolderIcon} from './components/Icons/Folder';
+import {GridIcon, ListIcon} from './components/Icons/Grid';
 import {PlusIcon} from './components/Icons/PlusIcon';
-import {EditorForm} from './components/PropertyEditor/EditorForm';
-import {Sidebar} from './components/Scaffold/Sidebar/Sidebar';
-import {sidebarLogo} from './components/Scaffold/Sidebar/Sidebar.css';
-import {Toolbar} from './components/Toolbar/Toolbar';
 import * as styles from './Dashboard.css';
 
 export type WorkspaceItemType = 'folder' | 'project';
@@ -33,55 +26,45 @@ export interface WorkspaceItem {
   lastUpdateDate: string;
 }
 
-function fetchFolders(): Promise<WorkspaceItem[]> {
-  return fetch('/workspace/folders').then(res => res.json());
-}
-
-function fetchFolderContent(id: string): Promise<WorkspaceItem[]> {
-  return fetch(`/workspace/folders/${id}`).then(res => res.json());
+function fetchWorkspaceContent(): Promise<WorkspaceItem[]> {
+  return fetch(`/workspace`).then(res => res.json());
 }
 
 export default function Dashboard() {
-  const [folders] = createResource(fetchFolders);
-  const [folderId, setFolderId] = createSignal<string>();
-  const params = useParams<{folderId: string}>();
-  const [data] = createResource(folderId, fetchFolderContent, {
+  const [data] = createResource(fetchWorkspaceContent, {
     initialValue: [],
   });
 
-  createEffect(on(() => params.folderId, setFolderId));
+  const [mode, setMode] = createSignal<'grid' | 'list'>('grid');
+  const tableModeItems: SegmentedFieldItem<'grid' | 'list'>[] = [
+    {
+      // @ts-expect-error Support custom elements in segmented field
+      label: <GridIcon />,
+      value: 'grid',
+    },
+    {
+      // @ts-expect-error Support custom elements in segmented field
+      label: <ListIcon />,
+      value: 'list',
+    },
+  ];
 
   return (
     <div class={styles.scaffold}>
       <Box display={'flex'} height={'100%'}>
-        <Sidebar position={'left'}>
-          <EditorForm>
-            <div class={sidebarLogo}>
-              <CodeImageLogo width={'70%'} />
-            </div>
-
-            <ul>
-              <For each={folders()}>
-                {folder => (
-                  <li>
-                    <Link href={`/dashboard/${folder.id}`}>{folder.name}</Link>
-                  </li>
-                )}
-              </For>
-            </ul>
-          </EditorForm>
-        </Sidebar>
-
         <div class={styles.wrapper}>
           <div class={styles.main}>
-            <Box display={'flex'}>
-              <h1 class={styles.title}>
-                Workspace /
-                <Box as={'span'} marginLeft={2}>
-                  {folders()?.find(folder => folder.id === folderId())?.name}
-                </Box>
-              </h1>
+            <Box display={'flex'} marginBottom={8}>
+              <h1 class={styles.title}>Workspace</h1>
               <div style={{flex: 1}} />
+              <Box display={'flex'} style={{width: '80px'}} marginRight={3}>
+                <SegmentedField
+                  size={'sm'}
+                  value={mode()}
+                  onChange={setMode}
+                  items={tableModeItems}
+                />
+              </Box>
               <Button theme="primary" variant="solid">
                 <PlusIcon size={'sm'} />
                 <Box as={'span'} marginLeft={2}>
@@ -90,7 +73,11 @@ export default function Dashboard() {
               </Button>
             </Box>
 
-            <ul class={styles.gridList}>
+            <Box
+              as={'ul'}
+              data-displayMode={mode()}
+              class={styles.gridList({displayMode: mode()})}
+            >
               <Suspense
                 fallback={
                   <>
@@ -142,7 +129,7 @@ export default function Dashboard() {
                   )}
                 </For>
               </Suspense>
-            </ul>
+            </Box>
           </div>
 
           <Footer />
