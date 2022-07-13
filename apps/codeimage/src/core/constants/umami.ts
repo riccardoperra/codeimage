f1d44c251b1ae3778fe3acbc4cdf984d373cfb00
riccardoperra <riccardo.perra@icloud.com>
2022-07-13 16:03:30 +0200

    fix: fix umami

--- apps/codeimage/src/core/constants/umami.ts
+++ apps/codeimage/src/core/constants/umami.ts
@@ -4,19 +4,24 @@ declare global {
   }
 }
 
+const isDev = import.meta.env.DEV;
+
 export const enableUmami = () => {
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   const umamiMock: Umami = function () {};
-  umamiMock.trackEvent = (event_value, event_type, url, website_id) => {
-    console.groupCollapsed(`[DEV] Umami track event`);
-    console.table([{event_value, event_type, website_id, url}]);
-    console.groupEnd();
-  };
-  umamiMock.trackView = (url, referrer, website_id) => {
-    console.groupCollapsed(`[DEV] Umami track view`);
-    console.table([{url, referrer, website_id}]);
-    console.groupEnd();
-  };
 
-  window.umami = window.umami || umamiMock;
+  if (isDev) {
+    umamiMock.trackEvent = (event_value, event_type, url, website_id) => {
+      console.groupCollapsed(`[DEV] Umami track event`);
+      console.table([{event_value, event_type, website_id, url}]);
+      console.groupEnd();
+    };
+    umamiMock.trackView = (url, referrer, website_id) => {
+      console.groupCollapsed(`[DEV] Umami track view`);
+      console.table([{url, referrer, website_id}]);
+      console.groupEnd();
+    };
+
+    window.umami = window.umami || umamiMock;
+  }
 };
