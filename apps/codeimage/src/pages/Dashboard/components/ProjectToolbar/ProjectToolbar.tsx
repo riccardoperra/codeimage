bc3db33568dc48881a5b675bf8e816110f7f2d7c
riccardoperra <riccardo.perra@icloud.com>
2022-07-25 23:10:35 +0200

    fix: fix loading overlay style

--- apps/codeimage/src/pages/Dashboard/components/ProjectToolbar/ProjectToolbar.tsx
+++ apps/codeimage/src/pages/Dashboard/components/ProjectToolbar/ProjectToolbar.tsx
@@ -1,5 +1,13 @@
-import {Box, Button, SegmentedField, SegmentedFieldItem} from '@codeimage/ui';
+import {
+  Box,
+  Button,
+  LoadingOverlay,
+  SegmentedField,
+  SegmentedFieldItem,
+} from '@codeimage/ui';
+import {useAsyncAction} from '@core/hooks/async-action';
 import {useNavigate} from 'solid-app-router';
+import {Show} from 'solid-js';
 import {GridIcon, ListIcon} from '../../../../components/Icons/Grid';
 import {PlusIcon} from '../../../../components/Icons/PlusIcon';
 import {getDashboardState} from '../../dashboard.state';
@@ -8,6 +16,7 @@ import * as styles from './ProjectToolbar.css';
 export function ProjectToolbar() {
   const navigate = useNavigate();
   const dashboard = getDashboardState()!;
+  const [data, {notify}] = useAsyncAction(createNew);
 
   async function createNew() {
     const result = await dashboard?.createNewProject();
@@ -28,6 +37,9 @@ export function ProjectToolbar() {
 
   return (
     <Box display={'flex'} marginBottom={8}>
+      <Show when={data.loading}>
+        <LoadingOverlay overlay={true} size={'3x'} />
+      </Show>
       <h1 class={styles.title}>Workspace</h1>
       <div style={{flex: 1}} />
       <Box display={'flex'} style={{width: '80px'}} marginRight={3}>
@@ -38,7 +50,7 @@ export function ProjectToolbar() {
           items={tableModeItems}
         />
       </Box>
-      <Button theme="primary" variant="solid" onClick={() => createNew()}>
+      <Button theme="primary" variant="solid" onClick={() => notify(0)}>
         <PlusIcon size={'sm'} />
         <Box as={'span'} marginLeft={2}>
           New
