a3b7cc8235ffbaa7624dc10ddee6a839ed502c68
riccardoperra <riccardo.perra@icloud.com>
2022-07-28 23:43:28 +0200

    fix: add support for Inter var

--- apps/codeimage/src/theme/global.css.ts
+++ apps/codeimage/src/theme/global.css.ts
@@ -1,11 +1,12 @@
 import {globalStyle} from '@vanilla-extract/css';
 
 globalStyle('body', {
-  fontFamily: 'Inter, system-ui, -apple-system, sans-serif',
+  fontFamily: 'Inter system-ui, -apple-system, sans-serif',
   '@supports': {
     '(font-variation-settings: normal)': {
       // Inter var
-      fontFamily: 'Inter, system-ui, -apple-system, sans-serif',
+      fontFamily: 'Inter var, system-ui, -apple-system, sans-serif',
+      fontFeatureSettings: '"cv02","cv03","cv04","cv11"',
     },
   },
 });
