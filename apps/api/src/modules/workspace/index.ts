74623f478ede0aa98e4ab13df898d95ef0e211fc
Richard Ortiz <richardortiz.kampaay.com>
2022-07-30 19:15:49 +0200

    feat(api): endpoint workspace
    poc
    
    Co-authored-by: hackpirodev <info@hackpiro.dev>

--- apps/api/src/modules/workspace/index.ts
+++ apps/api/src/modules/workspace/index.ts
@@ -1,10 +1,26 @@
-import {WorkspaceItem} from '@codeimage/prisma-models';
+import {
+  WorkspaceItem,
+  SnippetEditorOptions,
+  SnippetTerminal,
+  SnippetFrame,
+  SnippetEditorTab,
+} from '@codeimage/prisma-models';
 import fp from 'fastify-plugin';
 
 interface WorkspaceHandler {
   findAllByUserId(id: string): Promise<WorkspaceItem[]>;
+  deleteById(id: string): Promise<WorkspaceItem>;
+  create(data: WorkspaceRequest, userId: string): Promise<WorkspaceItem>;
+  updateById(data: WorkspaceItem): Promise<WorkspaceItem>;
 }
-
+type IdAndSnippetId = 'id' | 'snippetId';
+interface WorkspaceRequest {
+  name: WorkspaceItem['name'];
+  editorOptions: Omit<SnippetEditorOptions, IdAndSnippetId>;
+  terminal: Omit<SnippetTerminal, IdAndSnippetId>;
+  frame: Omit<SnippetFrame, IdAndSnippetId>;
+  editors: Omit<SnippetEditorTab, IdAndSnippetId>[];
+}
 export default fp(async fastify => {
   const handler: WorkspaceHandler = {
     findAllByUserId(id: string): Promise<WorkspaceItem[]> {
@@ -12,6 +28,52 @@ export default fp(async fastify => {
         where: {
           userId: id,
         },
+      });
+    },
+
+    deleteById(id: string): Promise<WorkspaceItem> {
+      return fastify.prisma.workspaceItem.delete({
+        where: {
+          id,
+        },
+      });
+    },
+    create(data: WorkspaceRequest, userId: string): Promise<WorkspaceItem> {
+      // const dati = fastify.prisma.workspaceItem.create({
+      //   data,
+      // });
+      return fastify.prisma.workspaceItem.create({
+        data: {
+          name: data.name,
+          userId: userId,
+          snippet: {
+            create: {
+              editorOptions: {
+                create: data.editorOptions,
+              },
+              terminal: {
+                create: data.terminal,
+              },
+              editorTabs: {
+                create: data.editors,
+              },
+              snippetFrame: {
+                create: data.frame,
+              },
+            },
+          },
+        },
+        include: {
+          snippet: true,
+        },
+      });
+    },
+    updateById(data: WorkspaceItem): Promise<WorkspaceItem> {
+      return fastify.prisma.workspaceItem.update({
+        where: {
+          id: data.id,
+        },
+        data,
       });
     },
   };
