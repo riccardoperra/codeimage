77231c4f9b30e271ef851d71095ff6829b02e01a
riccardoperra <riccardo.perra@icloud.com>
2022-07-31 23:08:55 +0200

    refactor(api): add schema ref

--- apps/api/src/modules/project/index.ts
+++ apps/api/src/modules/project/index.ts
@@ -1,12 +1,17 @@
 import {FastifyPluginAsync} from 'fastify';
 import {makePrismaProjectRepository} from './infra/prisma/prisma-project.repository';
 import {ProjectRepository} from './repository';
+import * as projectSchemas from './schema';
 
 export const project: FastifyPluginAsync = async fastify => {
-  await fastify.decorate(
+  fastify.decorate(
     'projectRepository',
     makePrismaProjectRepository(fastify.prisma),
   );
+
+  for (const schema of Object.values(projectSchemas)) {
+    fastify.addSchema(schema);
+  }
 };
 
 declare module 'fastify' {
