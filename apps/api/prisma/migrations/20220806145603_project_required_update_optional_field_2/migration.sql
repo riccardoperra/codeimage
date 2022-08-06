3969b72991f7e79e4271be932c0916867415d38c
riccardoperra <riccardo.perra@icloud.com>
2022-08-06 17:53:04 +0200

    fix: fix migrations

--- apps/api/prisma/migrations/20220806145603_project_required_update_optional_field_2/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "SnippetFrame" ALTER COLUMN "padding" SET DEFAULT 32,
-ALTER COLUMN "visible" SET DEFAULT true,
-ALTER COLUMN "opacity" SET DEFAULT 100;
-
--- AlterTable
-ALTER TABLE "SnippetTerminal" ALTER COLUMN "opacity" SET DEFAULT 100;
