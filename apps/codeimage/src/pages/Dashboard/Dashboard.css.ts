a46c869f9bcacd31929d1cce7c78d84ee24b1ed6
riccardoperra <riccardo.perra@icloud.com>
2022-08-11 21:42:43 +0200

    fix: fix lint

--- apps/codeimage/src/pages/Dashboard/Dashboard.css.ts
+++ apps/codeimage/src/pages/Dashboard/Dashboard.css.ts
@@ -1,4 +1,4 @@
-import {adaptiveFullScreenHeight, textStyles, themeVars} from '@codeimage/ui';
+import {adaptiveFullScreenHeight, themeVars} from '@codeimage/ui';
 import {style} from '@vanilla-extract/css';
 
 export const scaffold = style([
