dce20064cdbc04d30e3e7e37b6f7842359b9fd10
riccardoperra <riccardo.perra@icloud.com>
2022-08-11 21:01:35 +0200

    fix: fix api types

--- apps/codeimage/src/state/editor/editor.ts
+++ apps/codeimage/src/state/editor/editor.ts
@@ -1,4 +1,4 @@
-import {GetProjectByIdApi} from '@codeimage/api/api-types';
+import type * as ApiTypes from '@codeimage/api/api-types';
 import {
   createDerivedObservable,
   createDerivedSetter,
@@ -122,7 +122,7 @@ export function createEditorsStore() {
     () => filter(SUPPORTED_FONTS, font => font.id === state.options.fontId)[0],
   );
 
-  const setFromWorkspace = (item: GetProjectByIdApi['response']) => {
+  const setFromWorkspace = (item: ApiTypes.GetProjectByIdApi['response']) => {
     setEditors(
       item.editorTabs.map(
         editor =>
