8d6bd5c09fc9031a944159ae0464ee28b864d022
riccardoperra <riccardo.perra@icloud.com>
2022-08-14 12:17:59 +0200

    ci: fix ci preview

--- apps/codeimage/src/core/constants/auth0.ts
+++ apps/codeimage/src/core/constants/auth0.ts
@@ -5,6 +5,6 @@ const env = import.meta.env;
 export const auth0 = createAuth0Client({
   domain: env.VITE_PUBLIC_AUTH0_DOMAIN,
   client_id: env.VITE_PUBLIC_AUTH0_CLIENT_ID,
-  redirect_uri: env.VITE_PUBLIC_MY_CALLBACK_URL,
+  redirect_uri: window.location.href,
   audience: env.VITE_PUBLIC_AUTH0_AUDIENCE,
 });
