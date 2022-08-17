c607004b07904cac366d2b6902b904b1127ac875
riccardoperra <riccardo.perra@icloud.com>
2022-08-17 14:56:42 +0200

    ci: fix vercel rewrites

--- apps/codeimage/src/core/constants/auth0.ts
+++ apps/codeimage/src/core/constants/auth0.ts
@@ -5,6 +5,6 @@ const env = import.meta.env;
 export const auth0 = createAuth0Client({
   domain: env.VITE_PUBLIC_AUTH0_DOMAIN,
   client_id: env.VITE_PUBLIC_AUTH0_CLIENT_ID,
-  redirect_uri: window.location.href,
+  redirect_uri: `${window.location.protocol}/${window.location.host}`,
   audience: env.VITE_PUBLIC_AUTH0_AUDIENCE,
 });
