ca99a3efec6be7845b19e25cd757d4cdcecd8c33
riccardoperra <riccardo.perra@icloud.com>
2022-08-21 22:47:10 +0200

    chore: fix lint

--- packages/ui/src/lib/primitives/IconButton/IconButton.tsx
+++ packages/ui/src/lib/primitives/IconButton/IconButton.tsx
@@ -1,9 +1,13 @@
 import clsx from 'clsx';
+import {ValidConstructor} from 'solid-headless/dist/types/utils/dynamic-prop';
+import {ParentProps} from 'solid-js';
 import {omitProps} from 'solid-use';
 import {Button, ButtonProps} from '../Button/Button';
 import * as styles from './IconButton.css';
 
-export function IconButton(props: ButtonProps) {
+export function IconButton<T extends ValidConstructor = 'button'>(
+  props: ParentProps<ButtonProps<T>>,
+) {
   const classes = () => clsx(styles.iconButton, props.class);
 
   return (
