b36e0956f522b526c8a2c9e778bd293bf714b3c9
riccardoperra <riccardo.perra@icloud.com>
2022-08-21 12:58:40 +0200

    feat(ui): add loading and leftIcon support for button

--- packages/ui/src/lib/primitives/Button/Button.tsx
+++ packages/ui/src/lib/primitives/Button/Button.tsx
@@ -1,26 +1,57 @@
-import * as styles from './Button.css';
+import {elements} from '@solid-primitives/refs';
+import clsx from 'clsx';
 import {Button as ShButton} from 'solid-headless';
 import {ButtonProps as ShButtonProps} from 'solid-headless/dist/types/components/Button';
 import {ValidConstructor} from 'solid-headless/dist/types/utils/dynamic-prop';
-import {JSXElement} from 'solid-js';
+import {children, JSXElement, ParentProps, Show} from 'solid-js';
+import {omitProps} from 'solid-use';
+import {CustomComponentProps} from '../../utils';
+import {LoadingCircle} from '../Loader';
+import * as styles from './Button.css';
 
-type ButtonProps<T extends ValidConstructor = 'button'> = ShButtonProps<T> &
-  styles.ButtonVariants;
+type ButtonProps<T extends ValidConstructor = 'button'> = CustomComponentProps<
+  T,
+  {
+    leftIcon?: JSXElement;
+    loading?: boolean;
+  } & ShButtonProps<T> &
+    styles.ButtonVariants
+>;
 
 export function Button<T extends ValidConstructor = 'button'>(
-  props: ButtonProps<T>,
+  props: ParentProps<ButtonProps<T>>,
 ): JSXElement {
-  return (
-    <ShButton
-      {...props}
-      class={`${styles.buttonVariant({
+  const classes = () =>
+    clsx(
+      styles.buttonVariant({
         pill: props.pill,
         block: props.block,
         theme: props.theme,
         variant: props.variant,
         size: props.size || 'sm',
-      })} ${props.class || ''}`}
+        loading: props.loading,
+      }),
+      props.class,
+    );
+
+  const resolvedIcon = (): JSXElement => {
+    if (!props.leftIcon) return null;
+    const item = children(() => props.leftIcon);
+    const els = elements(item, SVGElement, HTMLElement);
+    // Should always be 1
+    els().forEach(el => el.classList.add(styles.buttonIcon));
+    return els();
+  };
+
+  return (
+    <ShButton
+      {...omitProps(props, ['class', 'leftIcon', 'loading'])}
+      class={classes()}
     >
+      <Show when={!!props.leftIcon && !props.loading}>{resolvedIcon()}</Show>
+      <Show when={props.loading}>
+        <LoadingCircle class={styles.buttonIcon} size={props.size ?? 'sm'} />
+      </Show>
       {props.children}
     </ShButton>
   );
