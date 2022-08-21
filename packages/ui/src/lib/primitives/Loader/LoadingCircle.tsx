79b6d7ac4c41695abedacbb34a1636117d56b0c1
riccardoperra <riccardo.perra@icloud.com>
2022-08-21 22:54:48 +0200

    chore: fix external side-effects

--- packages/ui/src/lib/primitives/Loader/LoadingCircle.tsx
+++ packages/ui/src/lib/primitives/Loader/LoadingCircle.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import {SvgIcon, SvgIconProps} from '../Icon';
+import {SvgIcon, SvgIconProps} from '../Icon/SvgIcon';
 import * as styles from './LoadingCircle.css';
 
 function LoadingCircleIcon(props: SvgIconProps) {
@@ -12,12 +12,12 @@ function LoadingCircleIcon(props: SvgIconProps) {
         r="10"
         stroke="currentColor"
         stroke-width="4"
-      ></circle>
+      />
       <path
         class={styles.ring}
         fill="currentColor"
         d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-      ></path>
+      />
     </SvgIcon>
   );
 }
