5d9f39e51581e53c36d2f271c5b60ef805268f94
riccardoperra <riccardo.perra@icloud.com>
2022-08-30 21:40:16 +0200

    chore: update icons

--- apps/codeimage/src/components/Icons/CloseIcon.tsx
+++ apps/codeimage/src/components/Icons/CloseIcon.tsx
@@ -5,16 +5,11 @@ export const CloseIcon = (props: SvgIconProps) => {
     <SvgIcon
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
-      viewBox="0 0 24 24"
+      viewBox="0 0 20 20"
       stroke="currentColor"
-      stroke-width="2"
       {...props}
     >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M6 18L18 6M6 6l12 12"
-      />
+      <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
     </SvgIcon>
   );
 };
--- apps/codeimage/src/components/Icons/Code.tsx
+++ apps/codeimage/src/components/Icons/Code.tsx
@@ -4,16 +4,14 @@ export const CodeIcon = (props: SvgIconProps) => {
   return (
     <SvgIcon
       xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      stroke-width="2"
+      viewBox="0 0 20 20"
+      fill="currentColor"
       {...props}
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
+        fill-rule="evenodd"
+        d="M6.28 5.22a.75.75 0 010 1.06L2.56 10l3.72 3.72a.75.75 0 01-1.06 1.06L.97 10.53a.75.75 0 010-1.06l4.25-4.25a.75.75 0 011.06 0zm7.44 0a.75.75 0 011.06 0l4.25 4.25a.75.75 0 010 1.06l-4.25 4.25a.75.75 0 01-1.06-1.06L17.44 10l-3.72-3.72a.75.75 0 010-1.06zM11.377 2.011a.75.75 0 01.612.867l-2.5 14.5a.75.75 0 01-1.478-.255l2.5-14.5a.75.75 0 01.866-.612z"
+        clip-rule="evenodd"
       />
     </SvgIcon>
   );
