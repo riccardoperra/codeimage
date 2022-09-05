f98a4bb99d775028fa8060bacda0165c75977dc9
riccardoperra <riccardo.perra@icloud.com>
2022-09-05 22:34:57 +0200

    fix: add custom cors plugin

--- apps/api/src/plugins/cors.ts
+++ apps/api/src/plugins/cors.ts
@@ -2,7 +2,12 @@ import fastifyCors from '@fastify/cors';
 import fp from 'fastify-plugin';
 
 export default fp(async fastify => {
-  const config = fastify.config.ALLOWED_ORIGINS ?? '';
+  const config = fastify.config.ALLOWED_ORIGINS;
+
+  if (!config) {
+    return;
+  }
+
   const origins = config.split(',').map(str => str.trim());
   const origin = origins.includes('*') ? '*' : origins;
 
