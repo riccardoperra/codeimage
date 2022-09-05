e0e810a905b947237df2ce367c440af027bb2b00
riccardoperra <riccardo.perra@icloud.com>
2022-09-05 22:48:22 +0200

    chore: typecheck fix

--- apps/codeimage/src/pages/Dashboard/components/DashboardHeader/DashboardHeader.tsx
+++ apps/codeimage/src/pages/Dashboard/components/DashboardHeader/DashboardHeader.tsx
@@ -18,7 +18,7 @@ export function DashboardHeader() {
           </div>
         </Box>
 
-        <Box class={actionBox} flexGroe={1}>
+        <Box class={actionBox} flexGrow={1}>
           <HStack spacing={'2'} marginLeft={'auto'}>
             <UserBadge />
           </HStack>
--- packages/ui/src/lib/primitives/Dialog/Dialog.tsx
+++ packages/ui/src/lib/primitives/Dialog/Dialog.tsx
@@ -41,7 +41,7 @@ export function Dialog(props: ParentProps<DialogProps>): JSXElement {
           initial={{opacity: 0, scale: 0.95}}
           animate={{opacity: 1, scale: 1}}
           exit={{opacity: 0, scale: 0.95}}
-          transition={{duration: 0.2, ease: 'ease-in-out'}}
+          transition={{duration: 0.2, easing: 'ease-in-out'}}
           {...overlayProps}
           {...dialogProps}
           {...modalProps}
