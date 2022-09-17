debfcd4833cf39a353846ec7a083f6054e2961ef
riccardoperra <riccardo.perra@icloud.com>
2022-09-17 22:02:54 +0200

    chore: fix healthcheck types

--- apps/api/src/plugins/healthcheck.ts
+++ apps/api/src/plugins/healthcheck.ts
@@ -1,8 +1,6 @@
-import * as fastifyHealthcheck from 'fastify-healthcheck';
+import fastifyHealthcheck from 'fastify-healthcheck';
 import fp from 'fastify-plugin';
 
 export default fp(async fastify => {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
   return fastify.register(fastifyHealthcheck);
 });
