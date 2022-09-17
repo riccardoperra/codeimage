1c40682bd0ffdf0b629cd304aeac47810160b704
riccardoperra <riccardo.perra@icloud.com>
2022-09-17 22:32:09 +0200

    chore: fix test

--- apps/api/src/plugins/healthcheck.ts
+++ apps/api/src/plugins/healthcheck.ts
@@ -1,6 +1,7 @@
-import fastifyHealthcheck from 'fastify-healthcheck';
+import * as fastifyHealthcheck from 'fastify-healthcheck';
 import fp from 'fastify-plugin';
 
 export default fp(async fastify => {
+  // @ts-expect-error ???
   return fastify.register(fastifyHealthcheck);
 });
