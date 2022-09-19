9defaf5e2b98d5950b6a4f0a4b37fa651ec0487d
riccardoperra <riccardo.perra@icloud.com>
2022-09-19 22:21:15 +0200

    chore: fix test coverage

--- apps/api/src/modules/project/schema/index.ts
+++ apps/api/src/modules/project/schema/index.ts
@@ -16,6 +16,7 @@ export {
   ProjectDeleteResponseSchema,
   ProjectDeleteResponse,
 } from './project-delete.schema';
+
 export {
   ProjectGetByIdResponseSchema,
   PartialProjectGetByIdResponseSchema,
