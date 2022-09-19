356877fb13825e1f85e71b99bae9b70bf1e6556e
riccardoperra <riccardo.perra@icloud.com>
2022-09-19 22:28:11 +0200

    chore: fix test coverage

--- apps/api/test/common/typebox/nullable.test.ts
+++ apps/api/test/common/typebox/nullable.test.ts
@@ -3,7 +3,7 @@ import t from 'tap';
 import {Nullable} from '../../../src/common/typebox/nullable';
 
 t.test('TypeBox nullable', async t => {
-  t.test('should return nullable', t => {
+  t.test('should return nullable', async t => {
     const result = Nullable(Type.String());
     t.match(result.anyOf, [
       {
