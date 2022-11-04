b79f7120436515b1d469d97ba41582ebaee2a910
riccardoperra <riccardo.perra@icloud.com>
2022-11-04 08:06:22 +0000

    feat(website): update main page

--- apps/website/src/components/Main/MainPage.css.ts
+++ apps/website/src/components/Main/MainPage.css.ts
@@ -1,11 +1,15 @@
 import {themeVars} from '@codeimage/ui';
+import { darkGrayScale } from '@codeimage/ui/themes/darkTheme';
 import {style} from '@vanilla-extract/css';
 
 export const main = style({
   display: 'flex',
   margin: 'auto',
+  flexDirection: "column",
   overflow: 'hidden',
   height: '100vh',
+  width:"100%",
+  background: "#090909"
 });
 
 export const imageBox = style({
@@ -18,10 +22,45 @@ export const textBox = style({
 });
 
 export const text = style({
-  width: '70%',
+  width: '50%',
+  textAlign: "center"
 });
 
-export const image = style({
-  transform: 'translate(0px, 100px)',
+export const imageLeft = style({
+  transform: 'translate(-50%, 0px)',
   position: 'absolute',
+  width: "75%",
+  left:"50%",
 });
+
+export const imageRight = style({
+  transform: 'translate(70%, 0px)',
+  position: 'absolute',
+  width: "75%",
+  left:"0",
+});
+
+export const backdrop = style({
+  position: "absolute",
+  height: "1000px",
+  width: "100%",
+  left: "50%",
+  top: "-1000px",
+  filter: "blur(250px)",
+  opacity: ".5",
+  backgroundImage: `linear-gradient(90deg, #0071ff, #ad00ff 48.44%, #0071ff)`,
+  overflow: "hidden"
+})
+
+export const screenshot = style({
+  display: "inline-block",
+  position: "relative",
+  "::before" :{
+    content: "",
+    width: "100%",
+    height: "px",
+    bottom: 0,
+    position: "absolute",
+    backgroundColor: themeVars.backgroundColor.blue[600]
+  }
+})
--- apps/website/src/components/Main/MainPage.tsx
+++ apps/website/src/components/Main/MainPage.tsx
@@ -1,34 +1,39 @@
 import {Box, Text} from '@codeimage/ui';
 import {
-  image,
+  imageLeft,
+  imageRight,
   imageBox,
   main,
   text,
   textBox,
+  backdrop,
+  screenshot,
 } from '~/components/Main/MainPage.css';
 
 export function MainPage() {
   return (
     <div class={main}>
+      <div class={backdrop}/>
       <Box
         display={'flex'}
         justifyContent={'center'}
-        marginTop={24}
+        marginTop={12}
         flexDirection={'column'}
         alignItems={'center'}
         class={textBox}
       >
         <Text weight={'bold'} size={'6xl'} class={text}>
-          Create beautiful screenshots of your source code.
+          Create beautiful <div class={screenshot}>screenshots</div> <br/> of your source code.
         </Text>
       </Box>
       <Box class={imageBox}>
         <img
-          class={image}
+          class={imageLeft}
           src={
             'https://user-images.githubusercontent.com/37072694/195017193-026527f0-a7ac-4151-90a8-bdae186f7e17.png'
           }
         />
+
       </Box>
     </div>
   );
