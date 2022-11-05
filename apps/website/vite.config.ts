6febad6e0b89060e0bd02778070bb2602ea681bf
riccardoperra <riccardo.perra@icloud.com>
2022-11-05 22:44:39 +0100

    ci(website): add ci

--- apps/website/vite.config.ts
+++ apps/website/vite.config.ts
@@ -1,10 +1,14 @@
 import {vanillaExtractPlugin} from '@vanilla-extract/vite-plugin';
 import solid from 'solid-start/vite';
 import {defineConfig} from 'vite';
+import {withStaticVercelPreview} from '../../scripts/vercel-output-build';
+
+const enableSsr = !process.env.ENABLE_VERCEL_BUILD;
 
 export default defineConfig({
   plugins: [
-    solid(),
+    solid({ssr: enableSsr}),
+    withStaticVercelPreview(),
     vanillaExtractPlugin({
       esbuildOptions: {
         external: ['solid-js/web'],
