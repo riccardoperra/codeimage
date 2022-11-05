b4e868a2f170f1f7e88fecef21b2f902488b2527
hackpirodev <info@hackpiro.dev>
2022-11-05 22:32:06 +0100

    feat(website): poc moving background

--- apps/website/src/components/Main/MainPage.css.ts
+++ apps/website/src/components/Main/MainPage.css.ts
@@ -1,6 +1,6 @@
 import {themeVars} from '@codeimage/ui';
 import {darkGrayScale} from '@codeimage/ui/themes/darkTheme';
-import {style} from '@vanilla-extract/css';
+import {keyframes, style} from '@vanilla-extract/css';
 
 export const main = style({
   display: 'flex',
@@ -34,6 +34,18 @@ export const imageLeft = style({
   zIndex: 1,
 });
 
+export const bgAnimation = keyframes({
+  '0%': {
+    backgroundPosition: '0% 50%',
+  },
+  '50%': {
+    backgroundPosition: '100% 50%',
+  },
+  '100%': {
+    backgroundPosition: '0% 50%',
+  },
+});
+
 export const backdrop = style({
   filter: 'blur(160px)',
   transform: 'translateZ(0px)',
@@ -41,11 +53,13 @@ export const backdrop = style({
   background:
     'conic-gradient(from 230.29deg at 51.63% 52.16%, rgb(36, 0, 255) 0deg, rgb(0, 135, 255) 67.5deg, rgb(108, 39, 157) 198.75deg, rgb(24, 38, 163) 251.25deg, rgb(54, 103, 196) 301.88deg, rgb(105, 30, 255) 360deg)',
   overflow: 'hidden',
+  backgroundSize: '400% 400%',
   width: '75%',
   height: '100%',
   position: 'relative',
   zIndex: 0,
   margin: 'auto',
+  animation: `${bgAnimation} 35s ease infinite`,
 });
 
 export const screenshot = style({
