c95bae1948730eaa17c8a6a81dc72fa66d9f0b8f
riccardoperra <riccardo.perra@icloud.com>
2022-11-06 18:52:13 +0100

    feat(website): add editor section parallax effect

--- apps/website/src/components/Features/EditorFeature.css.ts
+++ apps/website/src/components/Features/EditorFeature.css.ts
@@ -1,14 +1,62 @@
 import {themeVars} from '@codeimage/ui';
 import {style} from '@vanilla-extract/css';
 
-export const main = style({
+export const sectionWrapper = style({
   backgroundColor: '#000',
-  paddingTop: themeVars.spacing[24],
-  paddingBottom: themeVars.spacing[24],
+  position: 'relative',
+  display: 'flex',
+});
+
+export const content = style({
+  maxWidth: '80rem',
+  position: 'relative',
+  display: 'flex',
+  margin: 'auto',
+});
+
+export const editorSectionInfo = style({
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  paddingRight: themeVars.spacing[24],
+  position: 'relative',
+  overflow: 'hidden',
+  order: -1,
+});
+
+export const sectionContainer = style({
+  height: 'calc(70vh * 3)',
+  display: 'flex',
+  width: '100%',
+});
+
+export const textParallaxBox = style({
+  height: '70vh',
+  display: 'flex',
+  alignItems: 'center',
+});
+
+export const editorParallaxContent = style({
+  display: 'flex',
+  flexDirection: 'column',
+});
+
+export const scrollContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  top: 0,
 });
 
 export const editorImage = style({
   padding: themeVars.spacing[12],
+});
+
+export const editorImageSticky = style({
+  position: 'sticky',
+  height: '70vh',
+  top: 0,
+  width: `55%`,
+  display: 'flex',
+  alignItems: 'center',
 });
 
 export const editorImageCard = style({
--- apps/website/src/components/Features/EditorFeature.tsx
+++ apps/website/src/components/Features/EditorFeature.tsx
@@ -1,47 +1,112 @@
 import {Box, Text} from '@codeimage/ui';
-import {createResource} from 'solid-js';
+import {animate, scroll} from 'motion';
+import {onMount} from 'solid-js';
 import {
+  editorSectionInfo,
   editorImage,
   editorImageCard,
-  main,
+  editorImageSticky,
+  editorParallaxContent,
+  sectionWrapper,
+  content,
+  scrollContainer,
+  sectionContainer,
+  textParallaxBox,
 } from '~/components/Features/EditorFeature.css';
-import {container} from '~/components/Features/Features.css';
 
 export function EditorFeature() {
-  return (
-    <div class={main}>
-      <Box class={container}>
-        <Box display={'flex'}>
-          <Box style={{flex: 0.8}}>
-            <Box
-              marginTop={24}
-              marginBottom={24}
-              paddingRight={24}
-              paddingLeft={0}
-            >
-              <Text weight={'bold'} size={'5xl'}>
-                Custom editor
-              </Text>
+  let ref!: HTMLDivElement;
 
-              <Box marginTop={6}>
-                <Text size={'xl'} style={{'line-height': 1.5}}>
-                  CodeImage provide an editor with a set of defined
-                  configurations that helps you to create beautiful snippets of
-                  your source code in <strong>seconds</strong>.
-                </Text>
-              </Box>
-            </Box>
-          </Box>
-          <Box class={editorImageCard}>
+  onMount(() => {
+    const sections = ref.querySelectorAll(`.${textParallaxBox}`);
+    sections.forEach(item => {
+      scroll(animate(item, {opacity: [0, 1, 1, 0]}), {
+        target: item,
+        offset: ['start end', 'end end', 'start start', 'end start'],
+      });
+    });
+  });
+
+  return (
+    <section class={sectionWrapper} ref={ref}>
+      <div class={content}>
+        <div class={editorImageSticky}>
+          <div class={editorImageCard}>
             <img
               class={editorImage}
               src={
                 'https://user-images.githubusercontent.com/37072694/195017193-026527f0-a7ac-4151-90a8-bdae186f7e17.png'
               }
             />
-          </Box>
-        </Box>
-      </Box>
-    </div>
+          </div>
+        </div>
+        <div class={editorSectionInfo}>
+          <div class={scrollContainer}>
+            <div class={sectionContainer}>
+              <Box display={'flex'} class={editorParallaxContent}>
+                <div class={textParallaxBox}>
+                  <Box marginTop={24} marginBottom={24} paddingLeft={0}>
+                    <Text weight={'bold'} size={'5xl'}>
+                      Custom editor
+                    </Text>
+
+                    <Box marginTop={6}>
+                      <Text size={'xl'} style={{'line-height': 1.5}}>
+                        CodeImage provide an editor with a set of defined
+                        configurations that helps you to create beautiful
+                        snippets of your source code in <strong>seconds</strong>
+                        .
+                      </Text>
+                    </Box>
+                  </Box>
+                </div>
+                <Box class={textParallaxBox}>
+                  <Box
+                    marginTop={24}
+                    marginBottom={24}
+                    paddingRight={24}
+                    paddingLeft={0}
+                  >
+                    <Text weight={'bold'} size={'5xl'}>
+                      Built-in themes
+                    </Text>
+
+                    <Box marginTop={6}>
+                      <Text size={'xl'} style={{'line-height': 1.5}}>
+                        CodeImage provide a rich choice of known and custom
+                        themes.
+                        <p>
+                          Do you think some themes are missing? Just fill an
+                          issue on Github 😊
+                        </p>
+                      </Text>
+                    </Box>
+                  </Box>
+                </Box>
+                <Box class={textParallaxBox}>
+                  <Box
+                    marginTop={24}
+                    marginBottom={24}
+                    paddingRight={24}
+                    paddingLeft={0}
+                  >
+                    <Text weight={'bold'} size={'5xl'}>
+                      Third themes
+                    </Text>
+
+                    <Box marginTop={6}>
+                      <Text size={'xl'} style={{'line-height': 1.5}}>
+                        CodeImage provide an editor with a set of defined
+                        configurations that helps you to create beautiful.
+                      </Text>
+                    </Box>
+                  </Box>
+                </Box>
+              </Box>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
