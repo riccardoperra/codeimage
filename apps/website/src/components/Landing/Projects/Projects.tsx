ae4ba0541bd35f46f96527ed59825a21f4367807
riccardoperra <riccardo.perra@icloud.com>
2022-11-16 22:41:19 +0000

    fix: fix website hydration

--- apps/website/src/components/Landing/Projects/Projects.tsx
+++ apps/website/src/components/Landing/Projects/Projects.tsx
@@ -1,19 +1,19 @@
 import {Badge, Box, Button, SvgIcon, SvgIconProps, Text} from '@codeimage/ui';
 import {A} from '@solidjs/router';
 import {animate, scroll} from 'motion';
-import {onMount} from 'solid-js';
+import {JSX, onMount} from 'solid-js';
 import * as styles from './Projects.css';
 
-function StorageBox(props: SvgIconProps) {
+function StorageBox(props: JSX.IntrinsicElements['svg']) {
   return (
-    <SvgIcon viewBox="0 0 24 24" fill="currentColor" {...props}>
+    <svg viewBox="0 0 24 24" fill="currentColor" {...props}>
       <path d="M3.375 3C2.339 3 1.5 3.84 1.5 4.875v.75c0 1.036.84 1.875 1.875 1.875h17.25c1.035 0 1.875-.84 1.875-1.875v-.75C22.5 3.839 21.66 3 20.625 3H3.375z" />
       <path
         fill-rule="evenodd"
         d="M3.087 9l.54 9.176A3 3 0 006.62 21h10.757a3 3 0 002.995-2.824L20.913 9H3.087zm6.163 3.75A.75.75 0 0110 12h4a.75.75 0 010 1.5h-4a.75.75 0 01-.75-.75z"
         clip-rule="evenodd"
       />
-    </SvgIcon>
+    </svg>
   );
 }
 
@@ -33,7 +33,7 @@ export function Projects() {
         <div class={styles.card} ref={cardRef}>
           <div class={styles.content}>
             <Badge theme={styles.storageBadge}>
-              <StorageBox size={'lg'} />
+              <StorageBox width={26} height={26} />
             </Badge>
 
             <Box>
