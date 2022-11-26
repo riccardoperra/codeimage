f76ee44a1b2b4ce6003f7cf9229418bf25954e32
riccardoperra <riccardo.perra@icloud.com>
2022-11-26 21:09:38 +0100

    build(website): fix codemirror

--- apps/website/src/components/Landing/EditorSteps/CodeEditor/CodeEditor.tsx
+++ apps/website/src/components/Landing/EditorSteps/CodeEditor/CodeEditor.tsx
@@ -1,4 +1,9 @@
 import {EditorView} from '@codemirror/view';
+import {
+  createCodeMirror,
+  createEditorControlledValue,
+  createLazyCompartmentExtension,
+} from 'solid-codemirror';
 import {onMount} from 'solid-js';
 
 interface CodeEditorProps {
@@ -13,34 +18,32 @@ export function CodeEditor(props: CodeEditorProps) {
       .querySelector('[data-defer-font=codemirror]')
       ?.removeAttribute('media');
 
-    Promise.all([import('solid-codemirror')]).then(([cm]) => {
-      const {ref: setInternalRef, editorView} = cm.createCodeMirror({
-        value: props.code,
-      });
-      setInternalRef(() => ref);
-
-      cm.createEditorControlledValue(editorView, () => props.code);
-
-      cm.createLazyCompartmentExtension(
-        () =>
-          Promise.all([
-            Promise.resolve(
-              EditorView.theme({
-                '.cm-content': {
-                  fontFamily: 'Jetbrains Mono',
-                },
-              }),
-            ),
-            import('@codemirror/lang-javascript').then(({javascript}) =>
-              javascript({jsx: true, typescript: true}),
-            ),
-            import('@codeimage/highlight/synthwave84').then(
-              m => m.synthwave84Theme.editorTheme,
-            ),
-          ]),
-        editorView,
-      );
+    const {ref: setInternalRef, editorView} = createCodeMirror({
+      value: props.code,
     });
+    setInternalRef(() => ref);
+
+    createEditorControlledValue(editorView, () => props.code);
+
+    createLazyCompartmentExtension(
+      () =>
+        Promise.all([
+          Promise.resolve(
+            EditorView.theme({
+              '.cm-content': {
+                fontFamily: 'Jetbrains Mono',
+              },
+            }),
+          ),
+          import('@codemirror/lang-javascript').then(({javascript}) =>
+            javascript({jsx: true, typescript: true}),
+          ),
+          import('@codeimage/highlight/synthwave84').then(
+            m => m.synthwave84Theme.editorTheme,
+          ),
+        ]),
+      editorView,
+    );
   });
 
   return <div ref={ref}></div>;
