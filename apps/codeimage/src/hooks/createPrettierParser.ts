d97ce2ac95c5cb44e65be71278c1bfdf2797f451
riccardoperra <riccardo.perra@icloud.com>
2023-01-03 10:21:01 +0100

    fix: prettier parser improvements

--- apps/codeimage/src/hooks/createPrettierParser.ts
+++ apps/codeimage/src/hooks/createPrettierParser.ts
@@ -1,30 +1,75 @@
 import {SUPPORTED_LANGUAGES} from '@codeimage/config';
-import type {Options as PrettierOptions} from 'prettier';
+import type {
+  BuiltInParserName,
+  Options as PrettierOptions,
+  Plugin as PrettierPlugin,
+} from 'prettier';
 import {Accessor} from 'solid-js';
-import {createTabIcon} from './use-tab-icon';
+
+interface PrettierParserConfig {
+  parser?: BuiltInParserName | string | null;
+  parentParser?: BuiltInParserName | string;
+  plugins?: PrettierPlugin[] | null;
+}
+
+const unwrapPlugins = (
+  plugins: Promise<PrettierPlugin[] | PrettierPlugin> | undefined,
+) => {
+  if (!plugins) return [];
+  return plugins.then(plugin => {
+    return Array.isArray(plugin) ? plugin : [plugin];
+  });
+};
+
+async function makePrettierParser(
+  language: string,
+  tabName: string | null,
+): Promise<PrettierParserConfig> {
+  const selectedLanguage = SUPPORTED_LANGUAGES.find(
+    supportedLanguage => supportedLanguage.id === language,
+  );
+  if (!selectedLanguage) {
+    return {
+      parser: null,
+      plugins: [],
+      parentParser: undefined,
+    };
+  }
+
+  if (!tabName) {
+    return {
+      parser: selectedLanguage?.prettier?.parser ?? 'babel',
+      plugins: await unwrapPlugins(selectedLanguage?.prettier?.plugin?.()),
+    };
+  }
+
+  const matchedIcons =
+    selectedLanguage?.icons.filter(icon => icon.matcher.test(tabName)) ?? [];
+
+  const matchedIcon = matchedIcons[matchedIcons.length - 1];
+
+  if (!matchedIcon) {
+    return {
+      parser: selectedLanguage?.prettier?.parser ?? 'babel',
+      plugins: await unwrapPlugins(selectedLanguage?.prettier?.plugin?.()),
+    };
+  }
+
+  const prettier = matchedIcon?.prettier ?? selectedLanguage?.prettier;
+  return {
+    parentParser:
+      selectedLanguage.prettier?.parser !== matchedIcon.prettier?.parser
+        ? selectedLanguage?.prettier?.parser
+        : undefined,
+    parser: prettier?.parser,
+    plugins: await unwrapPlugins(prettier?.plugin?.()),
+  };
+}
 
 export function createPrettierParser(
   language: Accessor<string>,
   tabName: Accessor<string>,
 ) {
-  const languages = SUPPORTED_LANGUAGES;
-  const tabIcon = createTabIcon(tabName, language, false);
-
-  const currentLanguage = () => {
-    const id = language();
-    return languages.find(language => language.id === id);
-  };
-
-  const getParser = () => {
-    return tabIcon()?.prettier ?? currentLanguage()?.prettier;
-  };
-
-  const getParentParser = () => {
-    return getParser() !== currentLanguage()?.prettier?.parser
-      ? currentLanguage()?.prettier?.parser ?? undefined
-      : undefined;
-  };
-
   const getFormatter = () =>
     import('prettier').then(prettier => prettier.format);
   return {
@@ -33,27 +78,25 @@ export function createPrettierParser(
       options?: Omit<PrettierOptions, 'parser' | 'plugins'>,
     ) {
       const format = await getFormatter();
-      const languageDef = currentLanguage();
-      const languageIconDef = tabIcon();
+      const languageDef = language();
+      const languageIconDef = tabName();
       if (!languageDef && !languageIconDef) {
         throw new Error('Invalid language definition');
       }
-      const parser = getParser();
-      const parentParser = getParentParser();
+      const {parser, plugins, parentParser} = await makePrettierParser(
+        languageDef,
+        languageIconDef,
+      );
 
-      const plugins = await parser?.plugin?.();
-
-      const resolvedPlugins = plugins
-        ? Array.isArray(plugins)
-          ? plugins
-          : [plugins]
-        : [];
+      if (!parser || !plugins) {
+        throw new Error('Invalid parser configuration');
+      }
 
       return format(code, {
         ...(options || {}),
-        parser: parser?.parser,
-        plugins: resolvedPlugins,
-        parentParser,
+        parser,
+        plugins,
+        parentParser: parentParser ?? undefined,
       });
     },
   };
