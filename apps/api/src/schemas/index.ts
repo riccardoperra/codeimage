fc700d1d16c2bff83b6eac66ac71030eaa9bc628
Riccardo Perra <perrariccardo0@gmail.com>
2023-03-04 21:48:35 +0100

    feat(api): export route schemas

--- apps/api/src/schemas/index.ts
+++ apps/api/src/schemas/index.ts
@@ -4,3 +4,8 @@ export type {DeleteProjectApi} from '../routes/v1/project/delete';
 export type {UpdateProjectApi} from '../routes/v1/project/update';
 export type {GetProjectByIdApi} from '../routes/v1/project/getById';
 export type {CloneProjectApi} from '../routes/v1/project/clone';
+
+export type {GetPresetByIdApi} from '../routes/v1/preset/getById';
+export type {CreatePresetApi} from '../routes/v1/preset/create';
+export type {UpdatePresetApi} from '../routes/v1/preset/update';
+export type {DeletePresetApi} from '../routes/v1/preset/delete';
