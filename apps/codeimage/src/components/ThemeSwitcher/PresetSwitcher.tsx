26f0cf0494afbba709544adf3693db3752ef1360
Riccardo Perra <perrariccardo0@gmail.com>
2023-03-19 23:10:11 +0100

    feat: base PresetSwitcher component

--- apps/codeimage/src/components/ThemeSwitcher/PresetSwitcher.tsx
+++ apps/codeimage/src/components/ThemeSwitcher/PresetSwitcher.tsx
@@ -1,169 +1,220 @@
-import * as ApiTypes from '@codeimage/api/api-types';
 import {useI18n} from '@codeimage/locale';
 import {getRootEditorStore} from '@codeimage/store/editor';
+import {getFrameState} from '@codeimage/store/editor/frame';
 import {ProjectEditorPersistedState} from '@codeimage/store/editor/model';
+import {getTerminalState} from '@codeimage/store/editor/terminal';
 import {getThemeStore} from '@codeimage/store/theme/theme.store';
-import {useFilteredThemes} from '@codeimage/store/theme/useFilteredThemes';
-import {Box, FlexField, TextField} from '@codeimage/ui';
-import {useModality} from '@core/hooks/isMobile';
-import {assignInlineVars} from '@vanilla-extract/dynamic';
+import {getUiStore} from '@codeimage/store/ui';
 import {
-  createEffect,
-  createSelector,
-  createSignal,
-  For,
-  lazy,
-  ParentComponent,
-  Show,
-  Suspense,
-} from 'solid-js';
+  Box,
+  createStandaloneDialog,
+  DropdownMenuV2,
+  IconButton,
+  MenuButton,
+  Text,
+} from '@codeimage/ui';
+import {formatDistanceToNow} from '@core/helpers/date';
+import {Item} from '@solid-aria/collection';
+import {ConfirmDialog} from '@ui/ConfirmDialog/ConfirmDialog';
+import {RenameContentDialog} from '@ui/ConfirmDialog/RenameContentDialog';
+import {createResource, For, lazy, ParentComponent, Suspense} from 'solid-js';
 import {getAllPresets} from '../../data-access/preset';
 import {AppLocaleEntries} from '../../i18n';
-import {CheckCircle} from '../Icons/CheckCircle';
-import {EmptyCircle} from '../Icons/EmptyCircle';
+import {DotHorizontalIcon} from '../Icons/DotVertical';
 import {TerminalHost} from '../Terminal/TerminalHost';
+import * as styles2 from './PresetSwitcher.css';
 import {ThemeBox} from './ThemeBox';
 import {ThemeBoxSkeleton} from './ThemeBoxSkeleton';
 import * as styles from './ThemeSwitcher.css';
-import {gridSize, ThemeSwitcherVariant} from './ThemeSwitcher.css';
+import {ThemeSwitcherVariant} from './ThemeSwitcher.css';
 
 const CustomEditorPreview = lazy(() => {
   return import('../CustomEditor/CustomEditorPreview');
 });
 
 export const PresetSwitcher: ParentComponent<ThemeSwitcherVariant> = props => {
-  // const terminal = getTerminalState();
+  const terminal = getTerminalState();
   const editor = getRootEditorStore();
-  const modality = useModality();
-  const {themeArray, themeLoading} = getThemeStore();
+  const frame = getFrameState();
   const [t] = useI18n<AppLocaleEntries>();
-  const [filteredThemes, search, setSearch, isMatched] =
-    useFilteredThemes(themeArray);
-  const isSelected = createSelector(() => editor.state.options.themeId);
+  const locale = () => getUiStore().get.locale;
 
-  // TODO
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const onSelectTheme = (data: ProjectEditorPersistedState) => {
-    // TODO
-    // dispatchUpdateTheme({theme, updateBackground: true});
-    // getUmami().trackEvent(theme.id, `theme-change`);
+    editor.actions.setFromPersistedState(data.editor);
+    frame.setFromPersistedState(data.frame);
+    terminal.setFromPersistedState(data.terminal);
   };
 
+  const createDialog = createStandaloneDialog();
+
+  const [data] = createResource(() => getAllPresets({}));
+
   const exampleCode =
-    '// Just a code example \n' +
-    'export function Preview() {\n' +
-    ' const [count, setCount] = \n' +
+    'function Preview() {\n' +
+    ' const [get, set] = \n' +
     '   createSignal(0);\n' +
     '}';
-
-  const [data, setData] = createSignal<ApiTypes.GetAllPresetApi['response']>();
-
-  getAllPresets({}).then(setData);
-
-  createEffect(() => console.log(data()));
 
   return (
     <Box
       class={styles.grid({
         orientation: props.orientation,
       })}
-      style={assignInlineVars({
-        [gridSize]: data()?.length?.toString() ?? '0',
-      })}
     >
-      <Show when={modality === 'full'} keyed={false}>
-        <FlexField size={'lg'}>
-          <TextField
-            type={'text'}
-            disabled={themeLoading()}
-            placeholder={t('themeSwitcher.search')}
-            value={search()}
-            onChange={setSearch}
-          />
-        </FlexField>
-      </Show>
+      <Suspense
+        fallback={
+          <>
+            <ThemeBoxSkeleton />
+            <ThemeBoxSkeleton />
+          </>
+        }
+      >
+        <For each={data()}>
+          {theme => {
+            const data = () => theme.data as ProjectEditorPersistedState;
 
-      <For each={data()}>
-        {theme => {
-          return (
-            <Suspense fallback={<ThemeBoxSkeleton />}>
-              {(() => {
-                const data = () => theme.data as ProjectEditorPersistedState;
+            const themeRes = () =>
+              getThemeStore().getThemeResource(
+                data().editor.options.themeId,
+              )?.[0]?.();
 
-                const themeRes = () =>
-                  getThemeStore().getThemeResource(
-                    data().editor.options.themeId,
-                  )?.[0]?.();
+            const lastUpdateDate = () => {
+              return formatDistanceToNow(locale(), theme.updatedAt as string);
+            };
 
-                return (
-                  <div>
-                    <ThemeBox
-                      theme={{
-                        // TODO: to fix, we should abstract ThemeBox
-                        properties: {
-                          label: theme.name,
-                          previewBackground: data().frame.background!,
-                          // @ts-expect-error TODO to fix
-                          darkMode: themeRes()?.properties.darkMode,
-                          // @ts-expect-error TODO to fix
-                          terminal: {
-                            ...themeRes()?.properties.terminal,
-                          },
-                        },
-                      }}
-                      onClick={() => onSelectTheme(data())}
-                    >
-                      <TerminalHost
-                        textColor={data().terminal.textColor}
-                        background={data().terminal.background}
-                        accentVisible={data().terminal.accentVisible}
-                        shadow={data().terminal.shadow}
-                        showTab={data().terminal.alternativeTheme}
-                        readonlyTab={true}
-                        showHeader={data().terminal.showHeader}
-                        showWatermark={false}
-                        showGlassReflection={
-                          data().terminal.showGlassReflection
-                        }
-                        themeClass={styles.themeBoxTerminalHost}
-                        opacity={100}
-                        themeId={data().editor.options.themeId}
-                        alternativeTheme={data().terminal.alternativeTheme}
+            return (
+              <Suspense fallback={<ThemeBoxSkeleton />}>
+                {(() => {
+                  const data = () => theme.data as ProjectEditorPersistedState;
+
+                  return (
+                    <div>
+                      <li
+                        class={styles2.item}
+                        onClick={() => onSelectTheme(data())}
                       >
-                        <CustomEditorPreview
-                          themeId={data().editor.options.themeId}
-                          languageId={'typescript'}
-                          code={exampleCode}
-                        />
-                      </TerminalHost>
-                    </ThemeBox>
-
-                    <Box
-                      display={'flex'}
-                      justifyContent={'center'}
-                      marginTop={4}
-                    >
-                      <Show
-                        when={isSelected(theme.id)}
-                        fallback={
-                          <EmptyCircle
-                            cursor={'pointer'}
+                        <div>
+                          <ThemeBox
+                            showFooter={false}
+                            theme={{
+                              // TODO: to fix, we should abstract ThemeBox
+                              properties: {
+                                label: theme.name,
+                                previewBackground: data().frame.background!,
+                                // @ts-expect-error TODO to fix
+                                darkMode: themeRes()?.properties.darkMode,
+                                // @ts-expect-error TODO to fix
+                                terminal: {
+                                  ...themeRes()?.properties.terminal,
+                                },
+                              },
+                            }}
                             onClick={() => onSelectTheme(data())}
-                            size={'md'}
-                            opacity={0.35}
-                          />
-                        }
-                      >
-                        <CheckCircle size={'md'} />
-                      </Show>
-                    </Box>
-                  </div>
-                );
-              })()}
-            </Suspense>
-          );
-        }}
-      </For>
+                          >
+                            <TerminalHost
+                              textColor={data().terminal.textColor}
+                              background={data().terminal.background}
+                              accentVisible={data().terminal.accentVisible}
+                              shadow={data().terminal.shadow}
+                              showTab={data().terminal.alternativeTheme}
+                              readonlyTab={true}
+                              showHeader={data().terminal.showHeader}
+                              showWatermark={false}
+                              showGlassReflection={
+                                data().terminal.showGlassReflection
+                              }
+                              themeClass={styles.themeBoxTerminalHost}
+                              opacity={100}
+                              themeId={data().editor.options.themeId}
+                              alternativeTheme={
+                                data().terminal.alternativeTheme
+                              }
+                            >
+                              <CustomEditorPreview
+                                themeId={data().editor.options.themeId}
+                                languageId={'typescript'}
+                                code={exampleCode}
+                              />
+                            </TerminalHost>
+                          </ThemeBox>
+                        </div>
+
+                        <Box
+                          display={'flex'}
+                          justifyContent={'spaceBetween'}
+                          marginTop={4}
+                        >
+                          <div>
+                            <div>
+                              <Text weight={'semibold'}>{theme.name}</Text>
+                            </div>
+                            <Box marginTop={2}>
+                              <Text size={'xs'}>
+                                {t('dashboard.updated')} {lastUpdateDate()}
+                              </Text>
+                            </Box>
+                          </div>
+                          <div>
+                            <DropdownMenuV2
+                              onAction={(action: string | number) => {
+                                if (action === 'rename') {
+                                  createDialog(RenameContentDialog, state => ({
+                                    title: t(
+                                      'dashboard.renameProject.confirmTitle',
+                                    ),
+                                    message: t(
+                                      'dashboard.renameProject.confirmMessage',
+                                    ),
+                                    initialValue: theme.name,
+                                    onConfirm: async name => {
+                                      state.close();
+                                    },
+                                  }));
+                                }
+                                if (action === 'delete') {
+                                  createDialog(ConfirmDialog, state => ({
+                                    title: t(
+                                      'dashboard.deleteProject.confirmTitle',
+                                    ),
+                                    message: t(
+                                      'dashboard.deleteProject.confirmMessage',
+                                    ),
+                                    onConfirm: () => {
+                                      state.close();
+                                    },
+                                    actionType: 'danger' as const,
+                                  }));
+                                }
+                              }}
+                              menuButton={
+                                <MenuButton
+                                  as={IconButton}
+                                  variant={'solid'}
+                                  theme={'secondary'}
+                                  size={'xs'}
+                                >
+                                  <DotHorizontalIcon size={'md'} />
+                                </MenuButton>
+                              }
+                            >
+                              <Item key={'rename'}>
+                                {t('dashboard.renameProject.dropdownLabel')}
+                              </Item>
+                              <Item key={'delete'}>
+                                {t('dashboard.deleteProject.dropdownLabel')}
+                              </Item>
+                            </DropdownMenuV2>
+                          </div>
+                        </Box>
+                      </li>
+                    </div>
+                  );
+                })()}
+              </Suspense>
+            );
+          }}
+        </For>
+      </Suspense>
     </Box>
   );
 };
