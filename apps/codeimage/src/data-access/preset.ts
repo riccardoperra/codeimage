84fe3796539003acc11c37b22c0975d5a51ae093
Riccardo Perra <perrariccardo0@gmail.com>
2023-03-27 22:28:11 +0200

    feat: add preset store

--- apps/codeimage/src/data-access/preset.ts
+++ apps/codeimage/src/data-access/preset.ts
@@ -15,38 +15,6 @@ export async function deletePreset(
   }).then(res => res.json());
 }
 
-export async function updateSnippetName(
-  data: ApiTypes.UpdateProjectNameApi['request'],
-): Promise<ApiTypes.UpdateProjectNameApi['response']> {
-  return makeFetch(`${BASE_URL}/api/v1/project/:id/name`, {
-    method: 'PUT',
-    params: {
-      id: data.params?.id,
-    },
-    body: data.body,
-  }).then(res => res.json());
-}
-
-export async function getUserPresets(): Promise<
-  ApiTypes.GetPresetByIdApi['response'][]
-> {
-  return makeFetch(`${BASE_URL}/api/v1/project`, {
-    method: 'GET',
-  }).then(res => res.json());
-}
-
-export async function updateSnippet(
-  data: ApiTypes.UpdateProjectApi['request'],
-): Promise<ApiTypes.UpdateProjectApi['response']> {
-  return makeFetch(`${BASE_URL}/api/v1/project/:id`, {
-    method: 'PUT',
-    params: {
-      id: data.params?.id,
-    },
-    body: data.body,
-  }).then(res => res.json());
-}
-
 export async function createPreset(
   request: ApiTypes.CreatePresetApi['request'],
 ): Promise<ApiTypes.CreatePresetApi['response']> {
@@ -56,35 +24,20 @@ export async function createPreset(
   }).then(res => res.json());
 }
 
+export async function updatePreset(
+  request: ApiTypes.UpdatePresetApi['request'],
+): Promise<ApiTypes.UpdatePresetApi['response']> {
+  return makeFetch(`${BASE_URL}/api/v1/preset`, {
+    method: 'PUT',
+    body: request.body,
+  }).then(res => res.json());
+}
+
 export async function getAllPresets(
   request: ApiTypes.GetAllPresetApi['request'],
 ): Promise<ApiTypes.GetAllPresetApi['response']> {
   return makeFetch(`${BASE_URL}/api/v1/preset`, {
     method: 'GET',
-    body: request.body,
-  }).then(res => res.json());
-}
-
-export async function loadSnippet(
-  projectId: string,
-): Promise<ApiTypes.GetProjectByIdApi['response']> {
-  return makeFetch(`${BASE_URL}/api/v1/project/:id`, {
-    method: 'GET',
-    params: {
-      id: projectId,
-    },
-  }).then(res => res.json());
-}
-
-export async function cloneSnippet(
-  projectId: string,
-  request: ApiTypes.CloneProjectApi['request'],
-): Promise<ApiTypes.CloneProjectApi['response']> {
-  return makeFetch(`${BASE_URL}/api/v1/project/:id/clone`, {
-    method: 'POST',
-    params: {
-      id: projectId,
-    },
     body: request.body,
   }).then(res => res.json());
 }
