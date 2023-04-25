2b90bf57dbe343f36815be8576e1339380e0db73
Riccardo Perra <perrariccardo0@gmail.com>
2023-04-25 19:31:28 +0200

    fix: refactor unique id algorithm

--- apps/codeimage/src/state/plugins/unique-id.ts
+++ apps/codeimage/src/state/plugins/unique-id.ts
@@ -14,6 +14,9 @@ export function versionateId(id: string): string {
   return `${uid}$${parseInt(version, 10) + 1}`;
 }
 
-export const generateUid = () => {
-  return Date.now().toString(36) + Math.random().toString(36).substring(2);
+export const generateUid = (a = '') => {
+  return a
+    ? /* eslint-disable no-bitwise */
+      ((Number(a) ^ (Math.random() * 16)) >> (Number(a) / 4)).toString(16)
+    : `${1e7}-${1e3}-${4e3}-${8e3}-${1e11}`.replace(/[018]/g, generateUid);
 };
