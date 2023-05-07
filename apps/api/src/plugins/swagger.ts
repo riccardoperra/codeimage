de894a7848e5c83fe782415690e0ba93c2cf79a2
Riccardo Perra <perrariccardo0@gmail.com>
2023-05-07 12:04:36 +0200

    feat(api): add api swagger ui

--- apps/api/src/plugins/swagger.ts
+++ apps/api/src/plugins/swagger.ts
@@ -1,18 +1,27 @@
 import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUi from '@fastify/swagger-ui';
 import fp from 'fastify-plugin';
+import {version} from '../../package.json';
 
 export default fp(async fastify => {
   fastify.register(fastifySwagger, {
     swagger: {
       info: {
         title: 'CodeImage swagger',
-        description: 'Testing the Fastify swagger API',
-        version: '0.1.0',
+        description: 'CodeImage API Swagger',
+        version: version,
       },
-      host: 'localhost:3000',
-      schemes: ['http'],
+      schemes: ['http', 'https'],
       consumes: ['application/json'],
       produces: ['application/json'],
+    },
+  });
+
+  fastify.register(fastifySwaggerUi, {
+    routePrefix: '/api-docs',
+    uiConfig: {
+      docExpansion: 'full',
+      deepLinking: false,
     },
   });
 });
