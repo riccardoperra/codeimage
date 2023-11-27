e62d1983e4a802e9ac5c03a8ec78f13ba50f89e7
Riccardo Perra <riccardo.perra@icloud.com>
2023-11-27 22:51:04 +0000

    feat: update ts version

--- packages/ui/src/lib/primitives/IconButton/IconButton.tsx
+++ packages/ui/src/lib/primitives/IconButton/IconButton.tsx
@@ -1,17 +1,18 @@
-import {ElementType} from '@solid-aria/types';
+import { ElementType } from '@solid-aria/types';
 import clsx from 'clsx';
-import {ParentProps} from 'solid-js';
-import {omitProps} from 'solid-use';
-import {Button, ButtonProps} from '../Button/Button';
+import { ParentProps } from 'solid-js';
+import { omitProps } from 'solid-use';
+import { Button, ButtonProps } from '../Button/Button';
 import * as styles from './IconButton.css';
 
 export function IconButton<T extends ElementType = 'button'>(
   props: ParentProps<ButtonProps<T>>,
 ) {
   const classes = () =>
-    clsx(styles.iconButton({size: props.size}), props.class);
+    clsx(styles.iconButton({ size: props.size }), props.class);
 
   return (
+    // @ts-expect-error not valid type with TS>5.0
     <Button {...omitProps(props, ['class', 'children'])} class={classes()}>
       {props.children}
     </Button>
