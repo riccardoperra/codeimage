9810a9a29399f0419b77dac47df5903a8fcb2098
riccardoperra <riccardo.perra@icloud.com>
2024-04-20 20:17:00 +0200

    lint: fix lint

--- apps/codeimage/src/core/configuration.ts
+++ apps/codeimage/src/core/configuration.ts
@@ -12,11 +12,11 @@ export const [appEnvironment] = createConfiguration({
     max: 999_999,
   },
   editorPadding: [
-    {label: '0', value: "0"},
-    {label: '16', value: "16"},
-    {label: '32', value: "32"},
-    {label: '64', value: "64"},
-    {label: '128', value: "128"},
+    {label: '0', value: '0'},
+    {label: '16', value: '16'},
+    {label: '32', value: '32'},
+    {label: '64', value: '64'},
+    {label: '128', value: '128'},
   ],
   editorRadius: [
     {label: '0', value: 0},
