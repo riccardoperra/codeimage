2047723b1d241a4985a09437250d73ff01b4f15d
riccardoperra <riccardo.perra@icloud.com>
2024-04-27 11:42:00 +0200

    fix fleet dark dimension

--- packages/highlight/src/lib/themes/fleetDark/fleetDark.ts
+++ packages/highlight/src/lib/themes/fleetDark/fleetDark.ts
@@ -131,7 +131,6 @@ export const colors = EditorView.theme(
     '&': {
       color: tokens.gray120,
       background: tokens.gray10,
-      fontSize: '15px',
     },
     '&.cm-editor.cm-focused': {
       outline: 'none',
@@ -144,9 +143,6 @@ export const colors = EditorView.theme(
     '.cm-gutters': {
       backgroundColor: 'transparent',
       border: 'none',
-    },
-    '.cm-line': {
-      padding: '2px 2px 2px 12px',
     },
     '.cm-gutters .cm-gutter.cm-lineNumbers .cm-gutterElement': {
       color: tokens.gray70,
