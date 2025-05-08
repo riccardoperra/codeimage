11fdf441d9b0a6f6e4b384e772c47cd9899bb956
Riccardo Perra <riccardo.perra@icloud.com>
2025-05-08 10:15:25 +0200

    Update swagger.ts

--- apps/api/src/plugins/swagger.ts
+++ apps/api/src/plugins/swagger.ts
@@ -2,7 +2,7 @@ import fastifySwagger from '@fastify/swagger';
 import fastifySwaggerUi from '@fastify/swagger-ui';
 import fp from 'fastify-plugin';
 // @ts-expect-error IntelliJ may not support that
-import packageJson from '../../package.json' assert {type: 'json'};
+import packageJson from '../../package.json' with {type: 'json'};
 
 export default fp(async fastify => {
   fastify.register(fastifySwagger, {
