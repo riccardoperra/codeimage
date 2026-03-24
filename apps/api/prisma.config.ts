d7237a602f0aa224da29976f73f940b144cef3eb
riccardoperra <riccardo.perra@icloud.com>
2026-03-24 21:36:49 +0100

    generate prisma config fix
    Signed-off-by: riccardoperra <riccardo.perra@icloud.com>

--- apps/api/prisma.config.ts
+++ apps/api/prisma.config.ts
@@ -1,12 +1,17 @@
 import 'dotenv/config';
 import {defineConfig, env} from 'prisma/config';
+
+let databaseUrl: string | undefined = undefined;
+try {
+  databaseUrl = env('DATABASE_URL');
+} catch {}
+
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: {
     path: 'prisma/migrations',
   },
   datasource: {
-    url: env('DATABASE_URL'),
-    // shadowDatabaseUrl: env('SHADOW_DATABASE_URL'),
+    url: databaseUrl,
   },
 });
